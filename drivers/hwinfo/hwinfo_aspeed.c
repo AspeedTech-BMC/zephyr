3807fc0280c5129ad6bd54794a183d574bd112c6
Dylan Hung <dylan_hung@aspeedtech.com>
2021-03-08 15:31:53 +0800

    drivers:hwinfo: get scu base address from devicetree node
    instead of hardcoding scu base address, getting scu base from devicetree
    node "syscon" is more gerneral and flexiable.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>

--- drivers/hwinfo/hwinfo_aspeed.c
+++ drivers/hwinfo/hwinfo_aspeed.c
@@ -24,15 +24,14 @@ static struct soc_id soc_map_table[] = {
         SOC_ID("AST1030-A0", 0x8000000080000000),
 };
 
-#define SCU_BASE 0x7e6e2000
-
 ssize_t z_impl_hwinfo_get_device_id(uint8_t *buffer, size_t length)
 {
 	int i;
+	uint32_t base = DT_REG_ADDR(DT_NODELABEL(syscon));
 	uint64_t rev_id;
 
-	rev_id = sys_read32(SCU_BASE + ASPEED_REVISION_ID0);
-	rev_id = ((uint64_t)sys_read32(SCU_BASE + ASPEED_REVISION_ID1) << 32) |
+	rev_id = sys_read32(base + ASPEED_REVISION_ID0);
+	rev_id = ((uint64_t)sys_read32(base + ASPEED_REVISION_ID1) << 32) |
 		 rev_id;
 
 	for (i = 0; i < ARRAY_SIZE(soc_map_table); i++) {
