662f310a6c5ec2f1991b1786f91b67a59f543b64
Dylan Hung <dylan_hung@aspeedtech.com>
2021-03-09 14:15:04 +0800

    soc:arm: add aspeed soc cache initialization code
    the cachable region is defined by the devicetree node "sram_c".
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>

---
+++ soc/arm/aspeed/ast10x0/cache.c
@@ -0,0 +1,49 @@
+#include <stdint.h>
+#include <kernel.h>
+/**
+ * cache area control: each bit controls 32KB cache area
+ * 	1: cacheable
+ * 	0: no-cache
+ * 
+ * 	bit[0]: 1st 32KB from 0x0000_0000 to 0x0000_7fff
+ *	bit[1]: 2nd 32KB from 0x0000_8000 to 0x0000_ffff
+ *	...
+ *	bit[22]: 23th 32KB from 0x000a_8000 to 0x000a_ffff
+ *	bit[23]: 24th 32KB from 0x000b_0000 to 0x000b_ffff
+ */
+#define CACHE_AREA_CTRL_REG		0xa50
+#define CACHE_AREA_SIZE_LOG2		15
+#define CACHE_AREA_SIZE			(1 << CACHE_AREA_SIZE_LOG2)
+
+#define CACHE_INVALID_REG		0xa54
+#define DCACHE_INVALID(addr)		(BIT(31) | ((addr & GENMASK(10, 0)) << 16))
+#define ICACHE_INVALID(addr)		(BIT(15) | ((addr & GENMASK(10, 0)) << 0))
+
+#define CACHE_FUNC_CTRL_REG		0xa58
+#define ICACHE_CLEAN			BIT(2)
+#define DCACHE_CLEAN			BIT(1)
+#define CACHE_EANABLE			BIT(0)
+
+/* cache size = 32B * 128 = 4KB */
+#define CACHE_LINE_SIZE_LOG2		5
+#define CACHE_LINE_SIZE			(1 << CACHE_LINE_SIZE_LOG2)
+#define N_CACHE_LINE			128
+#define CACHE_ALIGNED_ADDR(addr)	((addr >> CACHE_LINE_SIZE_LOG2) << CACHE_LINE_SIZE_LOG2)
+
+/* prefetch buffer */
+#define PREFETCH_BUF_SIZE		CACHE_LINE_SIZE
+
+void aspeed_cache_init(void)
+{
+	uint32_t base = DT_REG_ADDR(DT_NODELABEL(syscon));
+	uint32_t sram_c_start = DT_REG_ADDR(DT_NODELABEL(sram_c));
+	uint32_t sram_c_end = sram_c_start + DT_REG_SIZE(DT_NODELABEL(sram_c)) - 1;
+	uint32_t start_bit, end_bit, config;
+
+	sys_write32(0, base + CACHE_FUNC_CTRL_REG);
+	start_bit = sram_c_start >> CACHE_AREA_SIZE_LOG2;
+	end_bit = sram_c_end >> CACHE_AREA_SIZE_LOG2;
+	config = GENMASK(end_bit, start_bit);
+	sys_write32(config, base + CACHE_AREA_CTRL_REG);
+	sys_write32(CACHE_EANABLE, base + CACHE_FUNC_CTRL_REG);
+}
--- soc/arm/aspeed/ast10x0/soc.c
+++ soc/arm/aspeed/ast10x0/soc.c
@@ -9,6 +9,10 @@
 #include <stdint.h>
 #include <linker/linker-defs.h>
 
+/* SCU registers */
+#define JTAG_PINMUX_REG	0x41c
+
+/* secure boot header : provide image size to bootROM for SPI boot */
 struct sb_header {
         uint32_t key_location;
         uint32_t enc_img_addr;
@@ -23,7 +27,8 @@ struct sb_header sbh __attribute((used, section(".sboot"))) = {
         .img_size = (uint32_t)&_image_rom_end,
 };
 
-#define JTAG_PINMUX_REG	0x41c
+/* external reference */
+void aspeed_cache_init(void);
 
 void z_platform_init(void)
 {
@@ -34,4 +39,7 @@ void z_platform_init(void)
 	jtag_pinmux = sys_read32(base + JTAG_PINMUX_REG);
 	jtag_pinmux |= (0x1f << 25);
 	sys_write32(jtag_pinmux, base + JTAG_PINMUX_REG);
+
+	/* init cache */
+	aspeed_cache_init();
 }
