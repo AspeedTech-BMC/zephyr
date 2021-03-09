f8dfed90a0bd9f42b2388a309638c77148bb2fc4
Billy Tsai <billy_tsai@aspeedtech.com>
2021-03-09 15:02:17 +0800

    pinmux: aspeed: Add pinmux driver for ast serial
    At this driver the pimux setting of all device will be configured at
    board init time.
    CPU side: we need to define all of the signal/pin(ball)
    file-> soc/arm/aspeed/astxxx/<pin_def_list.h, sig_def_list.h>
    Board side: we need to set which signal/function this board wants to use.
    And tell the framework how to bind signal to the pin and the signal
    combination of the function.
    file-> boards/arm/aspeed_xxx/fun_def_list.h
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>

---
+++ drivers/pinmux/aspeed/pinmux_aspeed.c
@@ -0,0 +1,229 @@
+/*
+ * Copyright (c) 2020-2021 Aspeed Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#define DT_DRV_COMPAT aspeed_ast1030_pinmux
+
+#include <drivers/pinmux.h>
+#include <kernel.h>
+
+#include <logging/log.h>
+LOG_MODULE_REGISTER(pimux_aspeed, LOG_LEVEL_DBG);
+#include "pinmux_aspeed.h"
+
+#ifdef CONFIG_PINCTRL_STRING_NAME
+
+#define PIN_DEFINE(pin) \
+    [pin] = # pin,
+const char *aspeed_pin_name[] =
+{
+    #include "pin_def_list.h"
+};
+#undef PIN_DEFINE
+
+#define FUN_DEFINE(fun, ...) \
+    [GET_FUN_ID(fun)] = # fun,
+const char *aspeed_fun_name[] =
+{
+    #include "fun_def_list.h"
+};
+#undef FUN_DEFINE
+
+#define SIG_DEFINE(sig, ...) \
+    [sig] = # sig,
+const char *aspeed_sig_name[] =
+{
+    #include "sig_def_list.h"
+};
+#undef SIG_DEFINE
+
+#endif
+
+#define SIG_DEFINE(sig, pin, ...) SIG_DECL(sig, pin, __VA_ARGS__);
+#include "sig_def_list.h"
+#undef SIG_DEFINE
+
+#define FUN_DEFINE(fun, ... ) FUN_DECL(fun, __VA_ARGS__);
+#include "fun_def_list.h"
+#undef FUN_DEFINE
+
+#define PIN_DEFINE(pin) \
+    [pin] = 0xffffffff,
+uint32_t aspeed_pin_desc_table[] =
+{
+    #include "pin_def_list.h"
+};
+#undef PIN_DEFINE
+
+#define SIG_DEFINE(sig, ...) \
+    [sig] = SIG_SYM_PTR(sig),
+const struct aspeed_sig_desc *aspeed_sig_desc_table[] =
+{
+	#include "sig_def_list.h"
+};
+#undef SIG_DEFINE
+
+#define FUN_DEFINE(fun, ...) \
+    [GET_FUN_ID(fun)] = FUN_SYM_PTR(fun),
+const struct aspeed_fun_desc *aspeed_fun_desc_table[] =
+{
+	#include "fun_def_list.h"
+};
+#undef FUN_DEFINE
+
+struct pinmux_aspeed_config {
+	uintptr_t base;
+};
+
+#define DEV_CFG(dev)					\
+	((const struct pinmux_aspeed_config * const)	\
+	 (dev)->config)
+
+/**
+ * aspeed_pinctrl_pin_status() - get the signal id of the occupied pin
+ * @pin_id: the pin id from pin_id.h
+ * @return  if > 0 :signal id, -1: the pin is free, -2: pin id Invalid
+ */
+static int pinmux_aspeed_get(const struct device *dev, uint32_t pin,
+			     uint32_t *func)
+{
+	ARG_UNUSED(dev);
+	if (pin >= MAX_PIN_ID || pin < 0) {
+		return -EINVAL;
+	}
+	*func = aspeed_pin_desc_table[pin];
+	return 0;
+}
+/**
+ * aspeed_pinctrl_sig_request() - request the signal. If the pin of the signal
+ * is free occupied it.
+ * @sig_id: the signal id from sig_id.h
+ * @return  hal status
+ */
+static int pinmux_aspeed_set(const struct device *dev, uint32_t pin,
+			     uint32_t func)
+{
+	uint32_t scu_base = DEV_CFG(dev)->base;
+	const struct aspeed_sig_desc *sig_desc;
+	const struct aspeed_sig_en *sig_en;
+	uint32_t ret_sig_id;
+	int sig_en_number;
+	int sig_en_idx;
+	if (func >=
+	    sizeof(aspeed_sig_desc_table) / sizeof(aspeed_sig_desc_table[0])) {
+		return -EINVAL;
+	}
+	sig_desc = aspeed_sig_desc_table[func];
+	if (sig_desc == NULL) {
+		return -EINVAL;
+	}
+
+	if (pin != sig_desc->pin) {
+		return -EINVAL;
+	}
+
+	pinmux_aspeed_get(dev, pin, &ret_sig_id);
+	if (ret_sig_id == 0xffffffff) {
+		aspeed_pin_desc_table[pin] = func;
+#ifdef CONFIG_PINCTRL_STRING_NAME
+		LOG_DBG("The pin %s is being occupied by signal %s\n",
+				 aspeed_pin_name[pin], aspeed_sig_name[func]);
+#else
+		LOG_DBG("The pin %d is being occupied by signal %d\n", pin, func);
+#endif
+		sig_en_number = sig_desc->nsig_en;
+		for (sig_en_idx = 0; sig_en_idx < sig_en_number; sig_en_idx++) {
+			sig_en = &sig_desc->sig_en[sig_en_idx];
+			if (sig_en->op) {
+				sys_write32(
+					(sys_read32(scu_base + sig_en->offset) &
+					 ~BIT(sig_en->bits)),
+					scu_base + sig_en->offset);
+			} else {
+				sys_write32(
+					(sys_read32(scu_base + sig_en->offset) |
+					 BIT(sig_en->bits)),
+					scu_base + sig_en->offset);
+			}
+		}
+		return 0;
+	} else {
+#ifdef CONFIG_PINCTRL_STRING_NAME
+		LOG_ERR("Request %s to pin %s error: already occupied by signal %s\n",
+				  aspeed_sig_name[func], aspeed_pin_name[pin],
+				  aspeed_sig_name[ret_sig_id]);
+#else
+		LOG_ERR("Request %d to pin %d error: already occupied by signal %d\n",
+				  func, pin, ret_sig_id);
+#endif
+		return -EBUSY;
+	}
+}
+
+static int pinmux_aspeed_pullup(const struct device *dev,
+		uint32_t pin, uint8_t func)
+{
+	return -ENOTSUP;
+}
+
+static int pinmux_aspeed_input(const struct device *dev,
+		uint32_t pin, uint8_t func)
+{
+	return -ENOTSUP;
+}
+
+/**
+ * aspeed_pinctrl_fn_group_request() - request all of the signal in the function group.
+ * @fun_id: the function group id from fun_id.h
+ * @return  hal status
+ */
+static int aspeed_pinctrl_fn_group_request(const struct device *dev, uint32_t fun_id)
+{
+	const struct aspeed_fun_desc *fun_desc;
+	const struct aspeed_sig_desc *sig_desc;
+	int sig_number;
+	int sig_idx;
+	uint16_t sig_id;
+	int ret;
+
+	fun_desc = aspeed_fun_desc_table[fun_id];
+	if (fun_desc == NULL) {
+		LOG_ERR("Invalid argument %d", fun_id);
+		return -EINVAL;
+	}
+	sig_number = fun_desc->nsignal;
+	for (sig_idx = 0; sig_idx < sig_number; sig_idx++) {
+		sig_id = fun_desc->sig_id_list[sig_idx];
+		sig_desc = aspeed_sig_desc_table[sig_id];
+		ret |= pinmux_aspeed_set(dev, sig_desc->pin, sig_id);
+	}
+	return ret;
+}
+
+static int pinmux_aspeed_init(const struct device *dev)
+{
+	uint32_t fun_id;
+	for (fun_id = 0; fun_id < MAX_FUN_ID; fun_id++) {
+		aspeed_pinctrl_fn_group_request(dev, fun_id);
+	}
+	return 0;
+}
+
+static const struct pinmux_driver_api pinmux_aspeed_driver_api = {
+	.set = pinmux_aspeed_set,
+	.get = pinmux_aspeed_get,
+	.pullup = pinmux_aspeed_pullup,
+	.input = pinmux_aspeed_input,
+};
+
+static const struct pinmux_aspeed_config pinmux_aspeed_config = {
+	.base = DT_REG_ADDR(DT_NODELABEL(syscon)),
+};
+
+
+DEVICE_DT_INST_DEFINE(0, &pinmux_aspeed_init, device_pm_control_nop,
+		    NULL, &pinmux_aspeed_config,
+		    PRE_KERNEL_1, CONFIG_PINMUX_INIT_PRIORITY,
+		    &pinmux_aspeed_driver_api);
---
+++ soc/arm/aspeed/ast10x0/sig_id.h
@@ -0,0 +1,13 @@
+#ifndef __SIG_ID_H__
+#define __SIG_ID_H__
+
+/* Pin ID */
+typedef enum {
+    SIG_NONE = -1,
+#define SIG_DEFINE(sig, ...) sig,
+    #include "sig_def_list.h"
+#undef SIG_DEFINE
+    MAX_SIG_ID,
+} aspeed_sig_id_t;
+
+#endif /* #ifndef __SIG_ID_H__ */
