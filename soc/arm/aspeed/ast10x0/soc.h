e29e20d08b1ef765be1adf277109f9c2a98a0d38
Dylan Hung <dylan_hung@aspeedtech.com>
2021-03-09 14:30:01 +0800

    soc:arm: add macro for declaring varibles in non-cached region
    example: uint32_t var NON_CACHED_BSS;
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>

--- soc/arm/aspeed/ast10x0/soc.h
+++ soc/arm/aspeed/ast10x0/soc.h
@@ -14,4 +14,8 @@
 #define __FPU_PRESENT  CONFIG_CPU_HAS_FPU
 #define __MPU_PRESENT  CONFIG_CPU_HAS_ARM_MPU
 
+/* non-cached (DMA) memory */
+#define NON_CACHED_BSS	__attribute__((section(".nocache.bss")))
+#define NON_CACHED_BSS_ALIGN16	__attribute__((aligned(16), section(".nocache.bss")))
+
 #endif /* ZEPHYR_SOC_ARM_ASPEED_AST10X0_SOC_H_*/
