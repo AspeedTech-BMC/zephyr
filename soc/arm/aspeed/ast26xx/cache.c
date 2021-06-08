da9598fe68e5833510670da3ab94678813da695c
Dylan Hung <dylan_hung@aspeedtech.com>
2021-06-08 14:23:40 +0800

    soc:arm:aspeed: add aspeed ast26xx series support
    add aspeed ast26xx soc support
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: I675824e9b6c354e6be8b351d2ab834c81f1a5fd5

--- soc/arm/aspeed/ast26xx/cache.c
+++ soc/arm/aspeed/ast26xx/cache.c
@@ -1,28 +1,28 @@
 #include <stdint.h>
 #include <kernel.h>
 /**
- * cache area control: each bit controls 32KB cache area
- * 	1: cacheable
- * 	0: no-cache
+ * cache area control: each bit controls 16MB cache area
+ *	1: cacheable
+ *	0: no-cache
  *
- * 	bit[0]: 1st 32KB from 0x0000_0000 to 0x0000_7fff
- *	bit[1]: 2nd 32KB from 0x0000_8000 to 0x0000_ffff
+ *	bit[0]: 1st 16MB from 0x0000_0000 to 0x00ff_ffff
+ *	bit[1]: 2nd 16MB from 0x0100_0000 to 0x01ff_ffff
  *	...
- *	bit[22]: 23th 32KB from 0x000a_8000 to 0x000a_ffff
- *	bit[23]: 24th 32KB from 0x000b_0000 to 0x000b_ffff
+ *	bit[30]: 31th 16MB from 0x1e00_0000 to 0x1eff_ffff
+ *	bit[31]: 32th 16MB from 0x1f00_0000 to 0x1fff_ffff
  */
-#define CACHE_AREA_CTRL_REG     0xa50
-#define CACHE_AREA_SIZE_LOG2    15
-#define CACHE_AREA_SIZE         (1 << CACHE_AREA_SIZE_LOG2)
+#define CACHE_AREA_CTRL_REG	0xa40
+#define CACHE_AREA_SIZE_LOG2	24
+#define CACHE_AREA_SIZE		(1 << CACHE_AREA_SIZE_LOG2)
 
-#define CACHE_INVALID_REG       0xa54
-#define DCACHE_INVALID(addr)    (BIT(31) | ((addr & GENMASK(10, 0)) << 16))
-#define ICACHE_INVALID(addr)    (BIT(15) | ((addr & GENMASK(10, 0)) << 0))
+#define CACHE_INVALID_REG	0xa44
+#define DCACHE_INVALID(addr)	(BIT(31) | ((addr & GENMASK(10, 0)) << 16))
+#define ICACHE_INVALID(addr)	(BIT(15) | ((addr & GENMASK(10, 0)) << 0))
 
-#define CACHE_FUNC_CTRL_REG     0xa58
-#define ICACHE_CLEAN            BIT(2)
-#define DCACHE_CLEAN            BIT(1)
-#define CACHE_EANABLE           BIT(0)
+#define CACHE_FUNC_CTRL_REG	0xa48
+#define ICACHE_CLEAN		BIT(2)
+#define DCACHE_CLEAN		BIT(1)
+#define CACHE_EANABLE		BIT(0)
 
 /* cache size = 32B * 128 = 4KB */
 #define CACHE_LINE_SIZE_LOG2    5
@@ -34,8 +34,8 @@
 /* prefetch buffer */
 #define PREFETCH_BUF_SIZE       CACHE_LINE_SIZE
 
-#define CACHED_SRAM_ADDR        DT_REG_ADDR_BY_IDX(DT_NODELABEL(sram0), 0)
-#define CACHED_SRAM_SIZE        DT_REG_SIZE_BY_IDX(DT_NODELABEL(sram0), 0)
+#define CACHED_SRAM_ADDR        DT_REG_ADDR_BY_IDX(DT_NODELABEL(sdram0), 0)
+#define CACHED_SRAM_SIZE        DT_REG_SIZE_BY_IDX(DT_NODELABEL(sdram0), 0)
 
 void aspeed_cache_init(void)
 {
---
+++ soc/arm/aspeed/ast26xx/soc.c
@@ -0,0 +1,57 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2021 ASPEED Technology Inc.
+ */
+
+#include <init.h>
+#include <kernel.h>
+#include <stdint.h>
+#include <linker/linker-defs.h>
+
+/*WDT0 registers*/
+#define WDT0_BASE 0x7e785000
+
+#define WDT_SOFTWARE_RESET_MASK_REG 0x28
+
+#define WDT_SOFTWARE_RESET_REG 0x24
+#define WDT_TRIGGER_KEY 0xAEEDF123
+
+/* secure boot header : provide image size to bootROM for SPI boot */
+struct sb_header {
+	uint32_t key_location;
+	uint32_t enc_img_addr;
+	uint32_t img_size;
+	uint32_t sign_location;
+	uint32_t header_rev[2];
+	uint32_t patch_location;        /* address of the rom patch */
+	uint32_t checksum;
+};
+
+struct sb_header sbh __attribute((used, section(".sboot"))) = {
+	.img_size = (uint32_t)&_image_rom_end,
+};
+
+/* external reference */
+void aspeed_cache_init(void);
+
+void z_platform_init(void)
+{
+	uint32_t base = DT_REG_ADDR(DT_NODELABEL(syscon));
+
+	/* FIXME:
+	 * for now we don't have clock-on function so put uart clock enabling
+	 * here.
+	 */
+	sys_write32(BIT(27), base + 0x94);
+
+	/* init cache */
+	aspeed_cache_init();
+}
+
+void sys_arch_reboot(int type)
+{
+	sys_write32(0x3fffff1, WDT0_BASE + WDT_SOFTWARE_RESET_MASK_REG);
+	sys_write32(WDT_TRIGGER_KEY, WDT0_BASE + WDT_SOFTWARE_RESET_REG);
+	ARG_UNUSED(type);
+}
