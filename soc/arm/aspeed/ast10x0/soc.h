a591dd3d504fb814dc776f18b9387691fb48fd09
Dylan Hung <dylan_hung@aspeedtech.com>
2021-06-09 17:05:19 +0800

    soc:arm:aspeed: add aspeed common include header file.
    put macros used for all aspeed soc family to aspeed_util.h
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: I3e776f966ac8ed3541a565a28e5a4c4a232424e5

--- soc/arm/aspeed/ast10x0/soc.h
+++ soc/arm/aspeed/ast10x0/soc.h
@@ -6,80 +6,17 @@
 
 #ifndef ZEPHYR_SOC_ARM_ASPEED_AST10X0_SOC_H_
 #define ZEPHYR_SOC_ARM_ASPEED_AST10X0_SOC_H_
+#include <aspeed_util.h>
 
-#include <sys/util.h>
-#include <devicetree.h>
+#define __CM4_REV		0x0001U
+#define __VTOR_PRESENT		1U
+#define __NVIC_PRIO_BITS	NUM_IRQ_PRIO_BITS
+#define __Vendor_SysTickConfig	0U
+#define __FPU_PRESENT		CONFIG_CPU_HAS_FPU
+#define __MPU_PRESENT		CONFIG_CPU_HAS_ARM_MPU
 
-#define DIV_ROUND_UP(n, d) (((n) + (d) - 1) / (d))
-
-#define reg_read_poll_timeout(map, reg, val, cond, sleep_tick, timeout_tick) \
-	({								     \
-		uint32_t __timeout_tick = (timeout_tick);		     \
-		uint32_t __start = z_tick_get_32();			     \
-		int __ret = 0;						     \
-		for (;;) {						     \
-			val.value = map->reg.value;			     \
-			if (cond)					     \
-			break;						     \
-			if ((z_tick_get_32() - __start) > __timeout_tick) {  \
-				__ret = -ETIMEDOUT;			     \
-				break;					     \
-			}						     \
-			if (sleep_tick)					     \
-			k_sleep (K_TICKS(sleep_tick));			     \
-		}							     \
-		__ret;							     \
-	})
-
-/* Common clock control device name for all Ast10x0 series */
-#define ASPEED_CLK_CTRL_NAME DT_INST_CLOCKS_LABEL(0)
-/* Common reset control device name for all Ast10x0 series */
-#define ASPEED_RST_CTRL_NAME DT_INST_RESETS_LABEL(0)
-
-/* non-cached (DMA) memory */
-#if (CONFIG_SRAM_NC_SIZE > 0)
-#define NON_CACHED_BSS	__attribute__((section(".nocache.bss")))
-#define NON_CACHED_BSS_ALIGN16	__attribute__((aligned(16), section(".nocache.bss")))
-#else
-#define NON_CACHED_BSS
-#define NON_CACHED_BSS_ALIGN16	__attribute__((aligned(16)))
-#endif /* end of "#if (CONFIG_SRAM_NC_SIZE > 0)" */
-/* CMSIS required definitions */
-typedef enum {
-	Reset_IRQn              = -15,
-	NonMaskableInt_IRQn     = -14,
-	HardFault_IRQn          = -13,
-#if defined(CONFIG_ARMV7_M_ARMV8_M_MAINLINE)
-	MemoryManagement_IRQn   = -12,
-	BusFault_IRQn           = -11,
-	UsageFault_IRQn         = -10,
-#if defined(CONFIG_ARM_SECURE_FIRMWARE)
-	SecureFault_IRQn        = -9,
-#endif  /* CONFIG_ARM_SECURE_FIRMWARE */
-#endif  /* CONFIG_ARMV7_M_ARMV8_M_MAINLINE */
-	SVCall_IRQn             =  -5,
-	DebugMonitor_IRQn       =  -4,
-	PendSV_IRQn             =  -2,
-	SysTick_IRQn            =  -1,
-/*
- * CMSIS IRQn_Type enum is broken relative to ARM GNU compiler.
- *
- * So add the MAX_IRQn to enlarge the size of IRQn_Type to avoid
- * ARM compiler from sign extending IRQn_Type values higher than 0x80
- * into negative IRQ values, which causes hard-to-debug Hard Faults.
- */
-	MAX_IRQn                = CONFIG_NUM_IRQS,
-} IRQn_Type;
-
-#define __CM4_REV                 0x0001U
-#define __VTOR_PRESENT            1U
-#define __NVIC_PRIO_BITS        NUM_IRQ_PRIO_BITS
-#define __Vendor_SysTickConfig    0U
-#define __FPU_PRESENT  CONFIG_CPU_HAS_FPU
-#define __MPU_PRESENT  CONFIG_CPU_HAS_ARM_MPU
-
-#define PHY_SRAM_ADDR		0x80000000UL
-#define TO_PHY_ADDR(addr)	(PHY_SRAM_ADDR + (uint32_t)(addr))
-#define TO_VIR_ADDR(addr)	((uint32_t)(addr) - PHY_SRAM_ADDR)
+#define PHY_SRAM_ADDR           0x80000000UL
+#define TO_PHY_ADDR(addr)       (PHY_SRAM_ADDR + (uint32_t)(addr))
+#define TO_VIR_ADDR(addr)       ((uint32_t)(addr) - PHY_SRAM_ADDR)
 
 #endif /* ZEPHYR_SOC_ARM_ASPEED_AST10X0_SOC_H_*/
