39920545ad169f18dc279c1389491072b0bd4952
Dylan Hung <dylan_hung@aspeedtech.com>
2021-06-09 11:51:42 +0800

    hwinfo: add ast26xx soc series information
    test on ast2600a3 evb
    
    Running test suite hwinfo_device_id_api
    ===================================================================
    START - test_device_id_get
    SOC: AST2600-A3
    SOC: AST2600-A3
    SOC: AST2600-A3
     PASS - test_device_id_get
    ===================================================================
    START - test_device_id_enotsup
     PASS - test_device_id_enotsup
    ===================================================================
    Test suite hwinfo_device_id_api succeeded
    ===================================================================
    PROJECT EXECUTION SUCCESSFUL
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: I51be8009b8d1bd4d09f5e9514b10deef93cbea8c

--- drivers/hwinfo/hwinfo_aspeed.c
+++ drivers/hwinfo/hwinfo_aspeed.c
@@ -10,18 +10,27 @@
 
 /* SoC mapping Table */
 
-#define ASPEED_REVISION_ID0	0x04
-#define ASPEED_REVISION_ID1	0x14
+#define ASPEED_REVISION_ID0     0x04
+#define ASPEED_REVISION_ID1     0x14
 
 #define SOC_ID(str, rev) { .name = str, .rev_id = rev, }
 
 struct soc_id {
-        const char *name;
-        uint64_t rev_id;
+	const char *name;
+	uint64_t rev_id;
 };
 
 static struct soc_id soc_map_table[] = {
-        SOC_ID("AST1030-A0", 0x8000000080000000),
+	SOC_ID("AST1030-A0", 0x8000000080000000),
+	SOC_ID("AST2600-A0", 0x0500030305000303),
+	SOC_ID("AST2600-A1", 0x0501030305010303),
+	SOC_ID("AST2620-A1", 0x0501020305010203),
+	SOC_ID("AST2600-A2", 0x0502030305010303),
+	SOC_ID("AST2620-A2", 0x0502020305010203),
+	SOC_ID("AST2605-A2", 0x0502010305010103),
+	SOC_ID("AST2600-A3", 0x0503030305030303),
+	SOC_ID("AST2620-A3", 0x0503020305030203),
+	SOC_ID("AST2605-A3", 0x0503010305030103),
 };
 
 ssize_t z_impl_hwinfo_get_device_id(uint8_t *buffer, size_t length)
@@ -35,14 +44,16 @@ ssize_t z_impl_hwinfo_get_device_id(uint8_t *buffer, size_t length)
 		 rev_id;
 
 	for (i = 0; i < ARRAY_SIZE(soc_map_table); i++) {
-		if (rev_id == soc_map_table[i].rev_id)
+		if (rev_id == soc_map_table[i].rev_id) {
 			break;
+		}
 	}
 
-	if (i == ARRAY_SIZE(soc_map_table))
+	if (i == ARRAY_SIZE(soc_map_table)) {
 		printk("UnKnow-SOC: %llx\n", rev_id);
-	else
+	} else {
 		printk("SOC: %4s \n", soc_map_table[i].name);
+	}
 
 	if (length > sizeof(rev_id)) {
 		length = sizeof(rev_id);
