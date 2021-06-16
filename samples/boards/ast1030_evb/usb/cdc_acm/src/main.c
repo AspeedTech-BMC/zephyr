57c20361d0533e8a70efadfa30b01d75054eea7d
neal_liu <neal_liu@aspeedtech.com>
2021-06-16 17:18:56 +0800

    samples: boards: ast1030_evb: usb: refine cdc acm sample
    Refine usb cdc acm sample to transfer large data.
    
    Signed-off-by: neal_liu <neal_liu@aspeedtech.com>
    Change-Id: If2a5b4f55a1db4465c9121432158bd80efa94238

--- samples/boards/ast1030_evb/usb/cdc_acm/src/main.c
+++ samples/boards/ast1030_evb/usb/cdc_acm/src/main.c
@@ -15,28 +15,55 @@
 #include <drivers/uart.h>
 #include <usb/usb_device.h>
 #include <logging/log.h>
+#include <sys/ring_buffer.h>
 
 LOG_MODULE_REGISTER(cdc_acm, LOG_LEVEL_INF);
 
-#define RX_BUFF_SIZE 1024
+#define RX_BUFF_SIZE	64
+#define RING_BUF_SIZE	KB(100)
+
+uint8_t ring_buffer[RING_BUF_SIZE];
+
+struct ring_buf ringbuf;
+int total_len;
 
 static void interrupt_handler(const struct device *dev, void *user_data)
 {
 	uint8_t rx_buff[RX_BUFF_SIZE];
-	int recv_len;
-	int i;
+	int recv_len, rb_len;
 
 	ARG_UNUSED(user_data);
 
 	while (uart_irq_is_pending(dev)) {
-		recv_len = uart_fifo_read(dev, rx_buff, RX_BUFF_SIZE);
+		uart_irq_rx_disable(dev);
+
+		recv_len = uart_fifo_read(dev, rx_buff, sizeof(rx_buff));
 		if (!recv_len)
-			return;
+			goto done;
 
 		LOG_INF("recv_len: 0x%x", recv_len);
-		for (i = 0; i < recv_len; i++)
+
+		rb_len = ring_buf_put(&ringbuf, rx_buff, recv_len);
+		if (rb_len < recv_len) {
+			LOG_ERR("Drop %u bytes", recv_len - rb_len);
+		}
+
+		total_len += recv_len;
+
+		rb_len = ring_buf_get(&ringbuf, rx_buff, sizeof(rx_buff));
+
+#ifdef RX_BUFF_PRINT
+		int i;
+
+		for (i = 0; i < rb_len; i++)
 			LOG_INF("rx_buff: 0x%x", rx_buff[i]);
+#endif
 	}
+
+	LOG_INF("total transfer len: 0x%x", total_len);
+
+done:
+	uart_irq_rx_enable(dev);
 }
 
 void main(void)
@@ -55,6 +82,8 @@ void main(void)
 		LOG_ERR("Failed to enable USB");
 		return;
 	}
+
+	ring_buf_init(&ringbuf, sizeof(ring_buffer), ring_buffer);
 
 	uart_irq_callback_set(dev, interrupt_handler);
 
