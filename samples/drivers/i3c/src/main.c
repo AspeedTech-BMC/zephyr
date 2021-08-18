86d7c2b30c5283465c76307b741aca425a4b0dd4
Dylan Hung <dylan_hung@aspeedtech.com>
2021-08-18 17:55:24 +0800

    samples: drivers: add i3c samples
    the sample code provides the demo of aspeed i3c master connecting with
    the Renesas IMX3102 mux device.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: Ibf17b0e666d47a3ecb9cb1d17c738dfdd52e03b8

---
+++ samples/drivers/i3c/src/main.c
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2021 ASPEED Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr.h>
+#include <sys/printk.h>
+#include <string.h>
+#include <device.h>
+#include <drivers/i3c/i3c.h>
+
+/* external reference */
+int i3c_aspeed_master_attach_device(const struct device *dev, struct i3c_device *slave);
+int i3c_aspeed_master_send_ccc(const struct device *dev, struct i3c_ccc_cmd *ccc);
+int i3c_aspeed_master_priv_xfer(struct i3c_device *i3cdev, struct i3c_priv_xfer *xfers, int nxfers);
+
+/* functions for sending CCC */
+static int i3c_send_rstdaa(const struct device *master)
+{
+	struct i3c_ccc_cmd ccc;
+
+	/* RSTDAA CCC */
+	ccc.addr = I3C_BROADCAST_ADDR;
+	ccc.id = I3C_CCC_RSTDAA;
+	ccc.payload.length = 0;
+	ccc.payload.data = NULL;
+	ccc.rnw = 0;
+	ccc.ret = 0;
+
+	return i3c_aspeed_master_send_ccc(master, &ccc);
+}
+
+static int i3c_send_setaasa(const struct device *master)
+{
+	struct i3c_ccc_cmd ccc;
+
+	ccc.addr = I3C_BROADCAST_ADDR;
+	ccc.id = I3C_CCC_SETAASA;
+	ccc.payload.length = 0;
+	ccc.payload.data = NULL;
+	ccc.rnw = 0;
+	ccc.ret = 0;
+
+	return i3c_aspeed_master_send_ccc(master, &ccc);
+}
+
+/**
+ * @brief data read for the JESD compliant devices
+ * @param slave the JESD compliant device
+ * @param add the address to be read
+ * @param buf buffer to store the read data
+ * @param length length of the read data
+ * @return -1 if the slave device is not registered
+ */
+int i3c_jesd_read(struct i3c_device *slave, uint8_t addr, uint8_t *buf, int length)
+{
+	struct i3c_priv_xfer xfer[2];
+	uint8_t mode_reg = addr;
+
+	if (!slave->master_dev) {
+		printk("unregistered device\n");
+		return -1;
+	}
+
+	xfer[0].rnw = 0;
+	xfer[0].len = 1;
+	xfer[0].data.out = &mode_reg;
+
+	xfer[1].rnw = 1;
+	xfer[1].len = length;
+	xfer[1].data.in = buf;
+
+	return i3c_aspeed_master_priv_xfer(slave, xfer, 2);
+}
+
+void main(void)
+{
+	const struct device *master;
+	struct i3c_device slave;
+	int ret;
+	uint8_t id[2];
+
+	master = device_get_binding(DT_LABEL(DT_NODELABEL(i3c0)));
+	if (!master) {
+		printk("master device not found\n");
+		goto test_fail;
+	}
+
+	slave.info.static_addr = 0xf;
+	slave.info.dynamic_addr = 0xf;
+	ret = i3c_aspeed_master_attach_device(master, &slave);
+	if (ret) {
+		printk("failed to attach slave\n");
+		goto test_fail;
+	}
+
+	ret = i3c_send_rstdaa(master);
+	if (ret) {
+		printk("RSTDAA failed %d\n", ret);
+		goto test_fail;
+	}
+
+	ret = i3c_send_setaasa(master);
+	if (ret) {
+		printk("SETAASA failed\n");
+		goto test_fail;
+	}
+
+	/* read device ID */
+	ret = i3c_jesd_read(&slave, 0, id, 2);
+	if (ret) {
+		printk("priv xfer failed\n");
+	}
+	printk("i3cdev id = %02x %02x\n", id[0], id[1]);
+
+test_fail:
+	while (1) {
+		k_sleep(K_MSEC(1000));
+	}
+}
