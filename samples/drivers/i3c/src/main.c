bf6e6225290b5ac689c804ecc379b50d7345820c
Dylan Hung <dylan_hung@aspeedtech.com>
2021-08-26 11:51:14 +0800

    samples/drivers/i3c: add i3c loopback sample
    to build the sample on ast2600 for example:
    west build -p auto -b ast2600_evb samples/drivers/i3c \
    -DDTC_OVERLAY_FILE="boards/ast2600_evb.overlay;loopback.overlay" \
    -DOVERLAY_CONFIG="overlay-loopback.conf"
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: I48a05073f2a3f5be57f6f91fc5a3a86ea2aa9307

--- samples/drivers/i3c/src/main.c
+++ samples/drivers/i3c/src/main.c
@@ -110,7 +110,8 @@ int i3c_i2c_read(struct i3c_device *slave, uint8_t addr, uint8_t *buf, int lengt
 	return i3c_aspeed_master_priv_xfer(slave, &xfer, 1);
 }
 
-void main(void)
+#ifdef CONFIG_I3C_SAMPLE_IMX3102
+void i3c_imx3112_test(void)
 {
 	const struct device *master;
 	struct i3c_device slave;
@@ -120,7 +121,7 @@ void main(void)
 	master = device_get_binding(DT_LABEL(DT_NODELABEL(i3c0)));
 	if (!master) {
 		printk("master device not found\n");
-		goto test_fail;
+		return;
 	}
 
 	/* Renesas IMX3102 */
@@ -129,40 +130,118 @@ void main(void)
 	slave.info.i2c_mode = 1;
 	ret = i3c_aspeed_master_attach_device(master, &slave);
 	if (ret) {
-		printk("failed to attach slave\n");
-		goto test_fail;
+		printk("failed to attach i2c slave\n");
+		return;
 	}
 
 	ret = i3c_send_rstdaa(master);
 	if (ret) {
 		printk("RSTDAA failed %d\n", ret);
-		goto test_fail;
+		return;
 	}
 
 	ret = i3c_i2c_read(&slave, 0, id, 2);
 	if (ret) {
 		printk("i2c xfer failed\n");
-		goto test_fail;
+		return;
 	}
 	printk("device ID in I2C mode %02x %02x\n", id[0], id[1]);
 
 	i3c_aspeed_master_deattach_device(master, &slave);
 	slave.info.i2c_mode = 0;
 	ret = i3c_aspeed_master_attach_device(master, &slave);
+	if (ret) {
+		printk("failed to attach i3c slave\n");
+		return;
+	}
+
 	ret = i3c_send_setaasa(master);
 	if (ret) {
 		printk("SETAASA failed\n");
-		goto test_fail;
+		return;
 	}
 	/* read device ID */
 	ret = i3c_jesd_read(&slave, 0, id, 2);
 	if (ret) {
-		printk("priv xfer failed\n");
-		goto test_fail;
+		printk("priv xfer failed %d\n", ret);
+		return;
 	}
 	printk("device ID in I3C mode %02x %02x\n", id[0], id[1]);
+}
+#endif
 
-test_fail:
+#ifdef CONFIG_I3C_SAMPLE_LOOPBACK
+/**
+ * @brief i3c0-to-i3c1 loopback test
+ *
+ * i3c0 plays the role as the main master and i3c1 plays the role as the slave device.
+ * Use SETAASA to assign the dynamic address.
+ *
+ */
+void i3c_loopback_test(void)
+{
+	const struct device *master;
+	struct i3c_device slave;
+	struct i3c_priv_xfer xfer;
+	int ret;
+	uint8_t data[4] = { 1, 2, 3, 4 };
+
+	master = device_get_binding(DT_LABEL(DT_NODELABEL(i3c0)));
+	if (!master) {
+		printk("master device not found\n");
+		return;
+	}
+
+	slave.info.static_addr = DT_PROP(DT_NODELABEL(i3c1), assigned_address);
+	slave.info.assigned_dynamic_addr = slave.info.static_addr;
+	slave.info.i2c_mode = 0;
+	ret = i3c_aspeed_master_attach_device(master, &slave);
+	if (ret) {
+		printk("failed to attach slave\n");
+		return;
+	}
+
+	/*
+	 * ASPEED IOP fix:
+	 * Aspeed slave device needs for a dummy CCC to activate the slave mode.
+	 * The slave device will NACK the dummy CCC so just ignore the first response in the master
+	 * side.
+	 */
+	ret = i3c_send_rstdaa(master);
+	ret = i3c_send_rstdaa(master);
+	if (ret) {
+		printk("RSTDAA failed %d\n", ret);
+		return;
+	}
+
+	ret = i3c_send_setaasa(master);
+	if (ret) {
+		printk("SETAASA failed\n");
+		return;
+	}
+	printk("bus init done\n");
+
+	xfer.rnw = 0;
+	xfer.len = 4;
+	xfer.data.out = data;
+	ret = i3c_aspeed_master_priv_xfer(&slave, &xfer, 1);
+	if (ret) {
+		printk("priv wr fail\n");
+	} else {
+		printk("priv wr pass\n");
+	}
+}
+#endif
+
+void main(void)
+{
+#ifdef CONFIG_I3C_SAMPLE_IMX3102
+	i3c_imx3112_test();
+#endif
+
+#ifdef CONFIG_I3C_SAMPLE_LOOPBACK
+	i3c_loopback_test();
+#endif
 	while (1) {
 		k_sleep(K_MSEC(1000));
 	}
