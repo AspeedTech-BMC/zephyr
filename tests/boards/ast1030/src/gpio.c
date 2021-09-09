05eff18f643751f78312e4485cb4d95df183dea5
Billy Tsai <billy_tsai@aspeedtech.com>
2021-09-09 10:55:30 +0800

    tests: ast1030: gpio: Move gpio basic test to CI.
    Support gpio test for CI, need to connect GPIO E0 to E1.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: Id8c320469736c51f57d2f92659976f6313d60886

--- tests/boards/ast1030/src/gpio.c
+++ tests/boards/ast1030/src/gpio.c
@@ -6,10 +6,15 @@
 
 #include <tc_util.h>
 #include "ast_test.h"
+#include "test_gpio/test_gpio.h"
 
 int test_gpio(int count, enum aspeed_test_type type)
 {
-	printk("%s, count: %d, type: %d\n", __func__, count, type);
+	ast_test_fail |= test_gpio_port();
+	ast_test_fail |= test_gpio_callback_add_remove();
+	ast_test_fail |= test_gpio_callback_self_remove();
+	ast_test_fail |= test_gpio_callback_enable_disable();
+	ast_test_fail |= test_gpio_callback_variants();
 
-	return 0;
+	return ast_ztest_result();
 }
