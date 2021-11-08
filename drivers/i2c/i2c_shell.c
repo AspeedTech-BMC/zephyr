1de906938e2a9fb150a3ea676e70a09a3acd6c84
tommy-huang <tommy_huang@aspeedtech.com>
2021-11-08 11:24:08 +0800

    drivers: i2c-shell: add i2c single read / write multi byte api
    Update i2c single read / write with multi-bytes api.
    
    Signed-off-by: tommy-huang <tommy_huang@aspeedtech.com>
    Change-Id: I367a6fa6dd13bb262df7666faf1d0a61966c6f33

--- drivers/i2c/i2c_shell.c
+++ drivers/i2c/i2c_shell.c
@@ -144,6 +144,48 @@ static int cmd_i2c_write(const struct shell *shell, size_t argc, char **argv)
 	return 0;
 }
 
+/* i2c write_bytes <device> <dev_addr> [<numbytes>] */
+static int cmd_i2c_write_multi_bytes(const struct shell *shell, size_t argc, char **argv)
+{
+	const struct device *dev;
+	int num_bytes;
+	int reg_addr;
+	int dev_addr;
+	int i;
+	uint8_t tx_buf[2];
+
+	dev = device_get_binding(argv[1]);
+	if (!dev) {
+		shell_error(shell, "I2C: Device driver %s not found.", argv[1]);
+		return -ENODEV;
+	}
+
+	dev_addr = strtol(argv[2], NULL, 16);
+	reg_addr = strtol(argv[3], NULL, 16);
+	tx_buf[0] = (uint8_t)reg_addr;
+	num_bytes = argc - 4;
+
+	if (num_bytes < 0) {
+		return 0;
+	}
+
+	if (num_bytes > MAX_I2C_BYTES) {
+		num_bytes = MAX_I2C_BYTES;
+	}
+
+	for (i = 0; i < num_bytes; i++) {
+		tx_buf[1] = (uint8_t)strtol(argv[4 + i], NULL, 16);
+		if (i2c_write(dev, tx_buf, 2, dev_addr) < 0) {
+			shell_error(shell, "Failed to mb write from device: %s", argv[1]);
+			return -EIO;
+		}
+		tx_buf[0]++;
+		k_msleep(8);
+	}
+
+	return 0;
+}
+
 static int cmd_i2c_write_byte(const struct shell *shell,
 			      size_t argc, char **argv)
 {
@@ -195,6 +237,53 @@ static int cmd_i2c_read_byte(const struct shell *shell,
 	}
 
 	shell_print(shell, "Output: 0x%x", out);
+
+	return 0;
+}
+
+/* i2c read_bytes <device> <dev_addr> [<numbytes>] */
+static int cmd_i2c_read_multi_bytes(const struct shell *shell, size_t argc, char **argv)
+{
+	uint8_t buf[MAX_I2C_BYTES];
+	const struct device *dev;
+	int num_bytes;
+	int reg_addr;
+	int dev_addr;
+	int i;
+	uint8_t tx_buf;
+
+	dev = device_get_binding(argv[1]);
+	if (!dev) {
+		shell_error(shell, "I2C: Device driver %s not found.", argv[1]);
+		return -ENODEV;
+	}
+
+	dev_addr = strtol(argv[2], NULL, 16);
+	reg_addr = strtol(argv[3], NULL, 16);
+	tx_buf = (uint8_t)reg_addr;
+
+	if (argc > 4) {
+		num_bytes = strtol(argv[4], NULL, 16);
+		if (num_bytes > MAX_I2C_BYTES)
+			num_bytes = MAX_I2C_BYTES;
+	} else {
+		num_bytes = MAX_I2C_BYTES;
+	}
+
+	for (i = 0; i < num_bytes; i++) {
+		if (i2c_write(dev, &tx_buf, 1, dev_addr) < 0) {
+			shell_error(shell, "Failed to mb write from device: %s", argv[1]);
+			return -EIO;
+		}
+
+		if (i2c_read(dev, &buf[i], 1, dev_addr) < 0) {
+			shell_error(shell, "Failed to mb read from device: %s", argv[1]);
+			return -EIO;
+		}
+		tx_buf++;
+	}
+
+	shell_hexdump(shell, buf, num_bytes);
 
 	return 0;
 }
@@ -892,12 +981,18 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_i2c_cmds,
 			       SHELL_CMD_ARG(read, &dsub_device_name,
 					     "Read bytes from an I2C device",
 					     cmd_i2c_read, 3, MAX_I2C_BYTES),
+				SHELL_CMD_ARG(read_sbyte, &dsub_device_name,
+					  "Read bytes from an I2C device with single",
+					  cmd_i2c_read_multi_bytes, 3, MAX_I2C_BYTES),
 			       SHELL_CMD_ARG(read_byte, &dsub_device_name,
 					     "Read a byte from an I2C device",
 					     cmd_i2c_read_byte, 3, 1),
 			       SHELL_CMD_ARG(write, &dsub_device_name,
 					     "Write bytes to an I2C device",
 					     cmd_i2c_write, 3, MAX_I2C_BYTES),
+				SHELL_CMD_ARG(write_sbyte, &dsub_device_name,
+					  "Write bytes to an I2C device with single",
+					  cmd_i2c_write_multi_bytes, 3, MAX_I2C_BYTES),
 			       SHELL_CMD_ARG(write_byte, &dsub_device_name,
 					     "Write a byte to an I2C device",
 					     cmd_i2c_write_byte, 4, 1),
