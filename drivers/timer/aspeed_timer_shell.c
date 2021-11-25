fe1496b68ee5180342e17fa73338d8afc294183e
Dylan Hung <dylan_hung@aspeedtech.com>
2021-11-25 17:28:35 +0800

    drivers/timer/aspeed: Use k_work to handle the timer event
    Because shell_print can not be called in ISR, create a worker to print
    the information to the users.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: Iccd10f8eabf5c4768c22df1fe93c0846cdbf868e

--- drivers/timer/aspeed_timer_shell.c
+++ drivers/timer/aspeed_timer_shell.c
@@ -11,6 +11,14 @@ static const char start_helper[] = "timer start <dev> -p <period> -t <type>";
 static const char stop_helper[] = "timer stop <dev>";
 static const char query_helper[] = "timer query <dev>";
 
+struct timer_shell_data {
+	const struct shell *shell;
+	const struct device *dev;
+	struct k_work work;
+};
+
+static struct timer_shell_data timer_shell_user_data;
+
 static void device_name_get(size_t idx, struct shell_static_entry *entry)
 {
 	const struct device *dev = shell_device_lookup(idx, TIMER_DEVICE_PREFIX);
@@ -22,11 +30,20 @@ static void device_name_get(size_t idx, struct shell_static_entry *entry)
 }
 SHELL_DYNAMIC_CMD_CREATE(dsub_device_name, device_name_get);
 
+static void timer_shell_worker(struct k_work *work)
+{
+	struct timer_shell_data *data = CONTAINER_OF(work, struct timer_shell_data, work);
+	const struct device *dev = data->dev;
+	const struct shell *shell = data->shell;
+
+	shell_print(shell, "%s expiration %d\n", dev->name, timer_aspeed_query(dev));
+}
+
 static void timer_shell_callback(void *user_data)
 {
-	struct device *dev = (struct device *)user_data;
+	struct timer_shell_data *data = (struct timer_shell_data *)user_data;
 
-	printk("%s expiration\n", dev->name);
+	k_work_submit(&data->work);
 }
 
 static int cmd_start(const struct shell *shell, size_t argc, char **argv)
@@ -70,9 +87,14 @@ static int cmd_start(const struct shell *shell, size_t argc, char **argv)
 		}
 	}
 
-	conf.callback = timer_shell_callback;
-	conf.user_data = (void *)dev;
+	if (!timer_shell_user_data.work.handler) {
+		k_work_init(&timer_shell_user_data.work, timer_shell_worker);
+	}
 
+	timer_shell_user_data.dev = dev;
+	timer_shell_user_data.shell = shell;
+	conf.callback = timer_shell_callback;
+	conf.user_data = &timer_shell_user_data;
 	shell_print(shell, "%s: period %d ms, type %d\n", dev->name, conf.millisec,
 		    conf.timer_type);
 	ret = timer_aspeed_start(dev, &conf);
