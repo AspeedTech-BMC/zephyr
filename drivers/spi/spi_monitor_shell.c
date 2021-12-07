c6f71fca9c464965caa782370471b1cffafb4615
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2021-12-07 11:41:23 +0800

    pfr: Create shell command environment for PFR
    - Add shell command environment for PFR related control.
    - Move BMC reset control command to PFR shell.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: I83a267d0660f4474650130e13e281164e318c4d8

--- drivers/spi/spi_monitor_shell.c
+++ drivers/spi/spi_monitor_shell.c
@@ -345,27 +345,6 @@ static int passthrough_mode_config(const struct shell *shell, size_t argc, char 
 	return 0;
 }
 
-static int bmc_rst_demo(const struct shell *shell, size_t argc, char *argv[])
-{
-
-	const struct device *spim_dev1 = NULL;
-
-	spim_dev1 = device_get_binding("spi_m1");
-	if (!spim_dev1) {
-		shell_error(shell, "cannot get the device, spi_m1.\n");
-		return -ENODEV;
-	}
-
-	/* SRST# */
-	pfr_bmc_rst_enable_ctrl(true);
-
-	spim_rst_flash(spim_dev1, 500);
-
-	pfr_bmc_rst_enable_ctrl(false);
-
-	return 0;
-}
-
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_spim_cmds,
 	SHELL_CMD_ARG(dump, NULL, "\"dump\"", dump_valid_cmd_table, 1, 0),
 	SHELL_CMD_ARG(add, NULL, "<command>", add_valid_cmd, 2, 1),
@@ -402,7 +381,6 @@ SHELL_STATIC_SUBCMD_SET_CREATE(spim_cmds,
 	SHELL_CMD(addr, &sub_spim_addr, "address privilege table related operations", NULL),
 	SHELL_CMD_ARG(lock, NULL, "lock", cmd_lock, 1, 0),
 	SHELL_CMD(config, &sub_spim_config, "SPI monitor configuration", NULL),
-	SHELL_CMD_ARG(rst_bmc, NULL, "reset BMC demo", bmc_rst_demo, 1, 0),
 
 	SHELL_SUBCMD_SET_END
 );
