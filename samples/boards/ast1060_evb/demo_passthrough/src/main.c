3ce83b1155da30e5d7392dfc84acb2178bee2dbf
tommy-huang <tommy_huang@aspeedtech.com>
2021-12-15 13:23:50 +0800

    sample: ast1060: Add i2c filter demo
    Move the i2c filter demo setting into main function.
    
    Signed-off-by: tommy-huang <tommy_huang@aspeedtech.com>
    Change-Id: I53c62766baa1ecb7c0216ef78a586d57f304fb33

--- samples/boards/ast1060_evb/demo_passthrough/src/main.c
+++ samples/boards/ast1060_evb/demo_passthrough/src/main.c
@@ -9,11 +9,51 @@
 #include <drivers/misc/aspeed/pfr_aspeed.h>
 #include "spi.h"
 #include "demo_gpio.h"
+#include <drivers/i2c.h>
+#include <drivers/i2c/pfr/i2c_filter.h>
 #include <drivers/watchdog.h>
 #include <soc.h>
 
 static struct k_work rst_work;
 static struct gpio_callback gpio_cb;
+static struct ast_i2c_f_bitmap data_flt[] = {
+{
+{	/* block all (index 0) */
+	0x00000000, 0x00000000, 0x00000000, 0x00000000,
+	0x00000000, 0x00000000, 0x00000000, 0x00000000
+}
+},
+{
+{	/* accept all (index 1) */
+	0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF
+}
+},
+{
+{	/* block every 16 byte (index 2) */
+	0xFFFF0000, 0xFFFF0000, 0xFFFF0000, 0xFFFF0000,
+	0xFFFF0000, 0xFFFF0000, 0xFFFF0000, 0xFFFF0000
+}
+},
+{
+{	/* block first 16 byte (index 3) */
+	0xFFFF0000, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF,
+	0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF
+}
+},
+{
+{	/* block first 128 byte (index 4) */
+	0x00000000, 0x00000000, 0x00000000, 0x00000000,
+	0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF
+}
+},
+{
+{	/* block last 128 byte (index 5) */
+	0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF,
+	0x00000000, 0x00000000, 0x00000000, 0x00000000,
+}
+}
+};
 
 void demo_spim_irq_init(void);
 void demo_wdt(void);
@@ -119,6 +159,50 @@ static void rst_gpio_event_register(void)
 	}
 }
 
+static void ast1060_i2c_demo_flt(void)
+{
+	uint8_t EEPROM_COUNT = 8;
+	uint8_t EEPROM_PASS_TBL[] = {0, 0, 0, 1, 1, 4, 4, 5};
+	const struct device *pfr_flt_dev = NULL;
+	int ret = 0, i = 0;
+
+	/* initial flt */
+	pfr_flt_dev = device_get_binding("I2C_FILTER_0");
+	if (!pfr_flt_dev) {
+		printk("I2C PFR : FLT Device driver not found.");
+		return;
+	}
+
+	if (pfr_flt_dev != NULL) {
+		ret = ast_i2c_filter_init(pfr_flt_dev);
+		if (ret) {
+			printk("I2C PFR : FLT Device Initial failed.");
+			return;
+		}
+
+		ret = ast_i2c_filter_en(pfr_flt_dev, 1, 1, 0, 0);
+		if (ret) {
+			printk("I2C PFR : FLT Device Enable / Disable failed.");
+			return;
+		}
+
+		ret = ast_i2c_filter_default(pfr_flt_dev, 0);
+		if (ret) {
+			printk("I2C PFR : FLT Device Set Default failed.");
+			return;
+		}
+
+		for (i = 0x0; i < EEPROM_COUNT; i++) {
+			ret = ast_i2c_filter_update(pfr_flt_dev, (uint8_t)(i),
+			(uint8_t) (0x50 + i), &data_flt[EEPROM_PASS_TBL[i]]);
+			if (ret) {
+				printk("I2C PFR : FLT Device Update failed.");
+				return;
+			}
+		}
+	}
+}
+
 void main(void)
 {
 	printk("%s demo\n", CONFIG_BOARD);
@@ -140,6 +224,10 @@ void main(void)
 	 */
 #if CONFIG_WDT_DEMO_ASPEED
 	demo_wdt();
+#endif
+
+#if CONFIG_I2C_PFR_FILTER
+	ast1060_i2c_demo_flt();
 #endif
 }
 
