ba2dab9874a29c521370c0cb73beeb13f9ec94ae
Dylan Hung <dylan_hung@aspeedtech.com>
2021-12-16 17:51:10 +0800

    boards: ast1030_evb: fix jlink runner
    Use "west attach" to halt the CPU and debug.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: Ief4e4ab722e03f5555d744bb8a57655e289d4f96

--- boards/arm/ast1030_evb/board.cmake
+++ boards/arm/ast1030_evb/board.cmake
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "-f")
+board_runner_args(jlink "--device=Cortex-M4" "--gdbserver=JLinkGDBServerCLExe")
 board_runner_args(dfu-util "--detach" "--pid=4153:1030" "--alt=1")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
