0b4f63554b7ce17c4350dad4f7922cda5ece4f46
Dylan Hung <dylan_hung@aspeedtech.com>
2021-12-23 11:03:36 +0800

    boards: ast2600_evb: Add Jlink board runner arg
    Add Jlink board runner args to support "west attach" command.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: Idc4d556dc020734467ab03d196aa14804e29eb5d

--- boards/arm/ast2600_evb/board.cmake
+++ boards/arm/ast2600_evb/board.cmake
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(jlink "-f")
+board_runner_args(jlink "--device=Cortex-M3" "--gdbserver=JLinkGDBServerCLExe")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
