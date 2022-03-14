acf489676acc3c0529b2f744de4c9fce854cc729
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2022-03-14 16:06:19 +0800

    pfr: Update GPIO config flow for BMC reset
    - Remove extra 10ms delay before holding BMC reset.
    - Set GPIO port value before configuring GPIO pin direction.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: I221b79cd49565f81bcd3f0421f9592a869e361fa

--- drivers/misc/aspeed/pfr_ast1060.c
+++ drivers/misc/aspeed/pfr_ast1060.c
@@ -26,16 +26,14 @@ void pfr_bmc_rst_enable_ctrl(bool enable)
 	const struct gpio_dt_spec gpio_m5 =
 		GPIO_DT_SPEC_GET_BY_IDX(DT_INST(0, demo_gpio_basic_api), out_gpios, 0);
 
-	ret = gpio_pin_configure_dt(&gpio_m5, GPIO_OUTPUT);
-	if (ret)
-		return;
-
-	k_busy_wait(10000); /* 10ms */
-
 	if (enable)
 		gpio_pin_set(gpio_m5.port, gpio_m5.pin, 0);
 	else
 		gpio_pin_set(gpio_m5.port, gpio_m5.pin, 1);
+
+	ret = gpio_pin_configure_dt(&gpio_m5, GPIO_OUTPUT);
+	if (ret)
+		return;
 
 	k_busy_wait(10000); /* 10ms */
 }
