935d3156195496ab83da266a58e026ed77604964
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2022-06-13 14:58:38 +0800

    abr: Get boot indicator support
    Create an API for getting boot indicator.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: I37343625def238f22ab3aed4389673e87add7034

--- drivers/misc/aspeed/abr_aspeed.c
+++ drivers/misc/aspeed/abr_aspeed.c
@@ -9,12 +9,21 @@
 #include <soc.h>
 #include <errno.h>
 #include <logging/log.h>
+#include <drivers/misc/aspeed/abr_aspeed.h>
 
 #define ABR_CTRL_REG	0x7e620064
 #define WDT_EVENT_CNT_CLEAR_MASK 0xff000000
 #define WDT_EVENT_CNT_CLEAR_MAGIC (0xea << 24)
 #define ABR_INDICATOR_CLEAR_MASK 0x00ff0000
 #define ABR_INDICATOR_CLEAR_MAGIC (0xea << 16)
+
+enum boot_indicator get_boot_indicator(void)
+{
+	if (sys_read32(ABR_CTRL_REG) & BIT(4))
+		return BOOT_FROM_ALTERNATE_PART;
+
+	return BOOT_FROM_PRIMARY_PART;
+}
 
 void disable_abr_wdt(void)
 {
