0f9eb1dfe52596d4c99f5e13f5141b6617cd8152
Dylan Hung <dylan_hung@aspeedtech.com>
2022-09-12 16:53:39 +0800

    clock: ast2600: refactor APB clock
    AST2600 has two APB clock sources for two dies so remove "PCLK" and use
    "APB1" and "APB2" to represent these two clocks.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: I5d2bb72480ab7328c888507cab2b2b691a3a0e38

--- drivers/clock_control/clock_control_ast26xx.c
+++ drivers/clock_control/clock_control_ast26xx.c
@@ -14,7 +14,31 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(clock_control_aspeed);
 
+/* fixed clock sources */
+#define CLKIN_FREQ			MHZ(25)
+#define HPLL_FREQ			MHZ(1200)
+#define HCLK_FREQ			MHZ(200)
+
+#define APLL_PARAM_REG			0x210
+#define   PLL_PARAM_BYPASS_MODE		BIT(24)
+#define   PLL_PARAM_P			GENMASK(22, 19)
+#define   PLL_PARAM_N			GENMASK(18, 13)
+#define   PLL_PARAM_M			GENMASK(12, 0)
+
+#define CLK_SELECTION_REG0		0x300
+#define   APB1_DIV_SEL			GENMASK(25, 23)
+#define     APB1_DIV_REG_TO_NUM(x)	((x + 1) * 4)
+
+#define CLK_SELECTION_REG4		0x310
+#define   APB2_DIV_SEL			GENMASK(11, 9)
+#define     APB2_DIV_REG_TO_NUM(x)	((x + 1) * 2)
+
 #define CLK_SELECTION_REG5		0x314
+#define   I3C_CLK_SRC_SEL		BIT(31)
+#define     I3C_CLK_SRC_HCLK		0
+#define     I3C_CLK_SRC_APLL_DIV	1
+#define   I3C_CLK_APLL_DIV_SEL		GENMASK(30, 28)
+#define     I3C_CLK_APLL_DIV_REG_TO_NUM(x) ((x == 0) ? 2 : (x + 1))
 
 struct clock_aspeed_config {
 	uintptr_t base;
@@ -64,11 +88,30 @@ static int aspeed_clock_control_off(const struct device *dev,
 	return 0;
 }
 
+static int aspeed_clock_control_get_pll_freq(uint32_t reg_value)
+{
+	uint32_t mult, div;
+
+	if (reg_value & PLL_PARAM_BYPASS_MODE) {
+		mult = div = 1;
+	} else {
+		uint32_t m = FIELD_GET(PLL_PARAM_M, reg_value);
+		uint32_t n = FIELD_GET(PLL_PARAM_N, reg_value);
+		uint32_t p = FIELD_GET(PLL_PARAM_P, reg_value);
+
+		mult = (m + 1) / (n + 1);
+		div = p + 1;
+	}
+
+	return (CLKIN_FREQ * mult / div);
+}
+
 static int aspeed_clock_control_get_rate(const struct device *dev,
 					 clock_control_subsys_t sub_system, uint32_t *rate)
 {
 	uint32_t clk_id = (uint32_t)sub_system;
-	uint32_t reg;
+	uint32_t base = DEV_CFG(dev)->base;
+	uint32_t reg, src, clk_div;
 
 	switch (clk_id) {
 	case ASPEED_CLK_GATE_I3C0CLK:
@@ -77,14 +120,28 @@ static int aspeed_clock_control_get_rate(const struct device *dev,
 	case ASPEED_CLK_GATE_I3C3CLK:
 	case ASPEED_CLK_GATE_I3C4CLK:
 	case ASPEED_CLK_GATE_I3C5CLK:
-		reg = sys_read32(DEV_CFG(dev)->base + CLK_SELECTION_REG5);
-		__ASSERT((reg & BIT(31)) == 0, "Not support I3C clock from APLL yet\n");
-		/* fall through */
+		reg = sys_read32(base + CLK_SELECTION_REG5);
+		if (FIELD_GET(I3C_CLK_SRC_SEL, reg) == I3C_CLK_SRC_APLL_DIV) {
+			src = aspeed_clock_control_get_pll_freq(sys_read32(base + APLL_PARAM_REG));
+			clk_div = I3C_CLK_APLL_DIV_REG_TO_NUM(FIELD_GET(I3C_CLK_APLL_DIV_SEL, reg));
+			*rate = src / clk_div;
+			break;
+		}
+		/* fall through if clock source is HCLK */
 	case ASPEED_CLK_HCLK:
-		*rate = 200000000;
+		*rate = HCLK_FREQ;
 		break;
-	case ASPEED_CLK_PCLK:
-		*rate = 50000000;
+	case ASPEED_CLK_APB1:
+		src = HPLL_FREQ;
+		reg = sys_read32(base + CLK_SELECTION_REG0);
+		clk_div = APB1_DIV_REG_TO_NUM(FIELD_GET(APB1_DIV_SEL, reg));
+		*rate = src / clk_div;
+		break;
+	case ASPEED_CLK_APB2:
+		src = HCLK_FREQ;
+		reg = sys_read32(base + CLK_SELECTION_REG4);
+		clk_div = APB2_DIV_REG_TO_NUM(FIELD_GET(APB2_DIV_SEL, reg));
+		*rate = src / clk_div;
 		break;
 	case ASPEED_CLK_GATE_UART1CLK:
 	case ASPEED_CLK_GATE_UART2CLK:
@@ -100,9 +157,6 @@ static int aspeed_clock_control_get_rate(const struct device *dev,
 	case ASPEED_CLK_GATE_UART12CLK:
 	case ASPEED_CLK_GATE_UART13CLK:
 		*rate = 24000000 / 13;
-		break;
-	case ASPEED_CLK_APB1:
-		*rate = 50000000;
 		break;
 	default:
 		LOG_ERR("Missing feature define for %d!", clk_id);
