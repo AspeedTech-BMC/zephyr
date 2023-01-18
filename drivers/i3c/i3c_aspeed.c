AspeedTech-BMC/zephyr

commit 7fb9a6ed08fa3c82ed0e7965ee0e06e10e07130c

i3c: aspeed: Separates the event of ibi and data consume.
The interrupt resp_q_ready indicate the event of ibi transfer done, data
read by master and master write data. The differnet of these events list
below:
1. if it include the signal ibi_update: ibi transfer done
2. if the response data contain the data length > 0: master write data.
3. others: data read by master.
This patch separates the wait event of the ibi and data consume, making
ISR can signal the appropriate event.

Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
Change-Id: I2f741d2031efc27d1611050db924116983f4ba64

drivers/i3c/i3c_aspeed.c
@@ -539,7 +539,8 @@ struct i3c_aspeed_obj {
 
 	/* slave mode data */
 	struct i3c_slave_setup slave_data;
-	osEventFlagsId_t event_id;
+	osEventFlagsId_t ibi_event;
+	osEventFlagsId_t data_event;
 };
 
 #define DEV_CFG(dev)			((struct i3c_aspeed_config *)(dev)->config)
@@ -672,25 +673,29 @@ static void i3c_aspeed_end_xfer(struct i3c_aspeed_obj *obj)
 	k_sem_give(&obj->curr_xfer->sem);
 }
 
-static void i3c_aspeed_slave_rx_data(struct i3c_aspeed_obj *obj)
+static void i3c_aspeed_slave_resp_handler(struct i3c_aspeed_obj *obj, union i3c_intr_s status)
 {
 	struct i3c_register_s *i3c_register = obj->config->base;
 	const struct i3c_slave_callbacks *cb;
 	uint32_t nresp, i;
 
 	cb = obj->slave_data.callbacks;
-	if (!cb) {
-		goto flush;
-	}
-
 	nresp = i3c_register->queue_status_level.fields.resp_buf_blr;
 	for (i = 0; i < nresp; i++) {
 		union i3c_device_resp_queue_port_s resp;
 		struct i3c_slave_payload *payload;
 
 		resp.value = i3c_register->resp_queue_port.value;
+		if (resp.fields.err_status) {
+			LOG_ERR("Respons Error: 0x%x\n", resp.fields.err_status);
+		}
+
 		if (resp.fields.data_length && !resp.fields.err_status &&
 		    resp.fields.tid == SLAVE_TID_MASTER_WRITE_DATA) {
+			if (!cb) {
+				__ASSERT(0, "flush rx fifo is TBD\n");
+				continue;
+			}
 			if (cb->write_requested) {
 				payload = cb->write_requested(obj->slave_data.dev);
 				payload->size = resp.fields.data_length;
@@ -700,11 +705,13 @@ static void i3c_aspeed_slave_rx_data(struct i3c_aspeed_obj *obj)
 			if (cb->write_done) {
 				cb->write_done(obj->slave_data.dev);
 			}
+		} else {
+			if (status.fields.ibi_update)
+				osEventFlagsSet(obj->ibi_event, status.value);
+			else
+				osEventFlagsSet(obj->data_event, status.value);
 		}
 	}
-	return;
-flush:
-	__ASSERT(0, "flush rx fifo is TBD\n");
 }
 
 static int i3c_aspeed_parse_ibi_status(uint32_t value, struct i3c_ibi_status *result)
@@ -845,13 +852,10 @@ static void i3c_aspeed_isr(const struct device *dev)
 			LOG_WRN("Master read when CMDQ is empty\n");
 
 		if (status.fields.resp_q_ready) {
-			i3c_aspeed_slave_rx_data(obj);
+			i3c_aspeed_slave_resp_handler(obj, status);
 		}
 
 		i3c_aspeed_slave_event(dev, status);
-
-		osEventFlagsSet(obj->event_id, status.value);
-
 	} else {
 		if (status.fields.resp_q_ready) {
 			i3c_aspeed_end_xfer(obj);
@@ -861,7 +865,6 @@ static void i3c_aspeed_isr(const struct device *dev)
 			i3c_aspeed_master_rx_ibi(obj);
 		}
 	}
-
 
 	if (status.fields.xfr_error) {
 		i3c_register->device_ctrl.fields.resume = 1;
@@ -1382,9 +1385,10 @@ static int i3c_aspeed_slave_wait_data_consume(const struct device *dev)
 	struct i3c_aspeed_obj *obj = DEV_DATA(dev);
 	union i3c_intr_s events;
 
+	osEventFlagsClear(obj->data_event, ~osFlagsError);
 	events.value = 0;
 	events.fields.resp_q_ready = 1;
-	osEventFlagsWait(obj->event_id, events.value, osFlagsWaitAny, osWaitForever);
+	osEventFlagsWait(obj->data_event, events.value, osFlagsWaitAny, osWaitForever);
 
 	return 0;
 }
@@ -1409,7 +1413,7 @@ int i3c_aspeed_slave_put_read_data(const struct device *dev, struct i3c_slave_pa
 			return -EACCES;
 		}
 
-		osEventFlagsClear(obj->event_id, ~osFlagsError);
+		osEventFlagsClear(obj->ibi_event, ~osFlagsError);
 		events.value = 0;
 		events.fields.ibi_update = 1;
 		events.fields.resp_q_ready = 1;
@@ -1443,7 +1447,7 @@ int i3c_aspeed_slave_put_read_data(const struct device *dev, struct i3c_slave_pa
 
 	if (ibi_notify) {
 		i3c_register->i3c_slave_intr_req.fields.sir = 1;
-		osEventFlagsWait(obj->event_id, events.value, osFlagsWaitAll, osWaitForever);
+		osEventFlagsWait(obj->ibi_event, events.value, osFlagsWaitAll, osWaitForever);
 	}
 
 	i3c_aspeed_slave_wait_data_consume(dev);
@@ -1469,7 +1473,7 @@ int i3c_aspeed_slave_send_sir(const struct device *dev, struct i3c_ibi_payload *
 		return -EACCES;
 	}
 
-	osEventFlagsClear(obj->event_id, ~osFlagsError);
+	osEventFlagsClear(obj->ibi_event, ~osFlagsError);
 	events.value = 0;
 	events.fields.ibi_update = 1;
 	events.fields.resp_q_ready = 1;
@@ -1491,7 +1495,7 @@ int i3c_aspeed_slave_send_sir(const struct device *dev, struct i3c_ibi_payload *
 
 	/* trigger the hw and wait done */
 	i3c_register->i3c_slave_intr_req.fields.sir = 1;
-	osEventFlagsWait(obj->event_id, events.value, osFlagsWaitAll, osWaitForever);
+	osEventFlagsWait(obj->ibi_event, events.value, osFlagsWaitAll, osWaitForever);
 
 	return 0;
 }
@@ -1710,7 +1714,8 @@ static int i3c_aspeed_init(const struct device *dev)
 
 	i3c_aspeed_enable(obj);
 
-	obj->event_id = osEventFlagsNew(NULL);
+	obj->ibi_event = osEventFlagsNew(NULL);
+	obj->data_event = osEventFlagsNew(NULL);
 
 	return 0;
 }

commit c27415bfbf0bd8d7eb170a624ac5f0f9cbafdc70

i3c: aspeed: Fixed the race issue with event flag
The event flag for pending read data consumption should be cleared
before triggering ibi to avoid the i3c slave have the race issue with
the i3c master (i.e., i3c slave should clear the event flag before the
master read).

Fixes: 7fb9a6ed08fa ("i3c: aspeed: Separates the event of ibi and data
consume.")
Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
Change-Id: Ic15484dff2ee02c928c49f287e75a8315fe598f6

drivers/i3c/i3c_aspeed.c
@@ -1385,18 +1385,6 @@ int i3c_aspeed_slave_register(const struct device *dev, struct i3c_slave_setup *
 	return 0;
 }
 
-static uint32_t i3c_aspeed_slave_wait_data_consume(const struct device *dev)
-{
-	struct i3c_aspeed_obj *obj = DEV_DATA(dev);
-	union i3c_intr_s events;
-
-	osEventFlagsClear(obj->data_event, ~osFlagsError);
-	events.value = 0;
-	events.fields.resp_q_ready = 1;
-
-	return osEventFlagsWait(obj->data_event, events.value, osFlagsWaitAny, K_SECONDS(3).ticks);
-}
-
 int i3c_aspeed_slave_put_read_data(const struct device *dev, struct i3c_slave_payload *data,
 				   struct i3c_ibi_payload *ibi_notify)
 {
@@ -1440,6 +1428,7 @@ int i3c_aspeed_slave_put_read_data(const struct device *dev, struct i3c_slave_pa
 		i3c_register->cmd_queue_port.value = cmd.value;
 	}
 
+	osEventFlagsClear(obj->data_event, ~osFlagsError);
 	if (config->priv_xfer_pec) {
 		xfer_buf = pec_append(dev, data->buf, data->size);
 		i3c_aspeed_wr_tx_fifo(obj, xfer_buf, data->size + 1);
@@ -1463,8 +1452,10 @@ int i3c_aspeed_slave_put_read_data(const struct device *dev, struct i3c_slave_pa
 			goto ibi_err;
 		}
 	}
-
-	flag_ret = i3c_aspeed_slave_wait_data_consume(dev);
+	events.value = 0;
+	events.fields.resp_q_ready = 1;
+	flag_ret =
+		osEventFlagsWait(obj->data_event, events.value, osFlagsWaitAny, K_SECONDS(3).ticks);
 	if (flag_ret & osFlagsError) {
 		i3c_aspeed_init(dev);
 		ret = -EIO;