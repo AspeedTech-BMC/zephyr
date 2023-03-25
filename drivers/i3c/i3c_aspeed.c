AspeedTech-BMC/zephyr

commit 563be112b0c68480debdd407c06b255a4a516863

i3c: aspeed: Use I2C FM speed for SETHID and DEVCTRL CCCs
According to JESD403 spec, send SETHID and DEVCTRL CCCs in I2C FM speed.
The implementation uses speed field of the command descriptor, instead
switch SCL PP mode setting in send_ccc function.

Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
Change-Id: I7a00298f6a81a5cf6683de2d7312c879bbdca58c

drivers/i3c/i3c_aspeed.c
@@ -80,6 +80,7 @@ union i3c_device_cmd_queue_port_s {
 #define COMMAND_PORT_SLAVE_DATA_CMD	0x0
 
 #define COMMAND_PORT_SPEED_I3C_SDR	0
+#define COMMAND_PORT_SPEED_I3C_I2C_FM	7
 #define COMMAND_PORT_SPEED_I2C_FM	0
 #define COMMAND_PORT_SPEED_I2C_FMP	1
 	struct {
@@ -1735,10 +1736,6 @@ int i3c_aspeed_master_send_ccc(const struct device *dev, struct i3c_ccc_cmd *ccc
 		}
 	}
 
-	if (ccc->id == I3C_CCC_SETHID) {
-		i3c_register->pp_timing.value = od_timing;
-	}
-
 	xfer.ncmds = 1;
 	xfer.cmds = &cmd;
 	xfer.ret = 0;
@@ -1766,6 +1763,9 @@ int i3c_aspeed_master_send_ccc(const struct device *dev, struct i3c_ccc_cmd *ccc
 	cmd_lo.xfer_cmd.roc = 1;
 	cmd_lo.xfer_cmd.rnw = ccc->rnw;
 	cmd_lo.xfer_cmd.toc = 1;
+	if (ccc->id == I3C_CCC_SETHID || ccc->id == I3C_CCC_DEVCTRL) {
+		cmd_lo.xfer_cmd.speed = COMMAND_PORT_SPEED_I3C_I2C_FM;
+	}
 	cmd.cmd_hi = cmd_hi.value;
 	cmd.cmd_lo = cmd_lo.value;
 

include/drivers/i3c/i3c.h
@@ -22,6 +22,7 @@
 #define I3C_CCC_ENTDAA          0x07
 #define I3C_CCC_SETAASA         0x29
 #define I3C_CCC_SETHID		0x61
+#define I3C_CCC_DEVCTRL		0x62
 
 /* unicast only commands */
 #define I3C_CCC_SETDASA         (0x7 | I3C_CCC_DIRECT)

commit dddda2cb3e1127e60b43bbbb3424c86ec93cb153

i3c: Remove missing SCL PP timing setting from send_ccc
Fix the missing register push of the SCL PP timing.

Fixes: 563be112b0c6 ("i3c: aspeed: Use I2C FM speed for SETHID and DEVCTRL CCCs")

Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
Change-Id: Ieecb554bd765fd08c32af5e55afd1562e2a54728

drivers/i3c/i3c_aspeed.c
@@ -1778,10 +1778,6 @@ int i3c_aspeed_master_send_ccc(const struct device *dev, struct i3c_ccc_cmd *ccc
 
 	ret = xfer.ret;
 
-	if (ccc->id == I3C_CCC_SETHID) {
-		i3c_register->pp_timing.value = pp_timing;
-	}
-
 	return ret;
 }
 