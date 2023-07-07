f8f36ad59d90327de848d04eacd08bbffd281c42
Dylan Hung <dylan_hung@aspeedtech.com>
2023-07-07 17:08:57 +0800

    drivers: pinctrl: Add Aspeed pinctrl driver
    Add Aspeed pin control driver. Only support pinmux for now.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: I390aca18ecd616ef28b320f7c9f0c23e954d1f07

---
+++ drivers/pinctrl/pinctrl_aspeed.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2023 ASPEED Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#define DT_DRV_COMPAT aspeed_pinctrl
+
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/pinctrl.h>
+#include <zephyr/drivers/syscon.h>
+#include <zephyr/dt-bindings/pinctrl/ast10x0-pinctrl.h>
+#include <pinctrl_soc.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(pinctrl_aspeed, LOG_LEVEL_INF);
+
+#define PINCTRL_SIG_DESC_OP		BIT(31)
+#define   PINCTRL_SIG_DESC_SET		0
+#define   PINCTRL_SIG_DESC_CLR		1
+#define PINCTRL_SIG_DESC_BIT_INDEX	GENMASK(27, 20)
+#define PINCTRL_SIG_DESC_REG_OFFSET	GENMASK(19, 0)
+
+static int pinctrl_configure_pin(const pinctrl_soc_pin_t *pin)
+{
+	const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(syscon));
+	uint32_t op, index, offset, value;
+	int i, ret = 0;
+
+#ifdef CONFIG_PINCTRL_ASPEED_STRING_NAME
+	LOG_INF("name %s, ball %d", pin->name, pin->ball);
+#else
+	LOG_INF("ball %d", pin->ball);
+#endif
+
+	for (i = 0; i < pin->num_of_descs; i++) {
+		op = FIELD_GET(PINCTRL_SIG_DESC_OP, pin->sig_descs[i]);
+		index = FIELD_GET(PINCTRL_SIG_DESC_BIT_INDEX, pin->sig_descs[i]);
+		offset = FIELD_GET(PINCTRL_SIG_DESC_REG_OFFSET, pin->sig_descs[i]);
+
+		ret = syscon_read_reg(dev, offset, &value);
+		if (op == PINCTRL_SIG_DESC_CLR) {
+			value &= ~BIT(index);
+		} else {
+			value |= BIT(index);
+		}
+		ret = syscon_write_reg(dev, offset, value);
+	}
+
+	return ret;
+}
+
+int pinctrl_configure_pins(const pinctrl_soc_pin_t *pins, uint8_t pin_cnt, uintptr_t reg)
+{
+	int i, ret = 0;
+
+	for (i = 0; i < pin_cnt; i++) {
+		ret = pinctrl_configure_pin(&pins[i]);
+		if (ret) {
+			LOG_ERR("Failed to configure pin %d\n", pins[i].ball);
+			break;
+		}
+	}
+
+	return ret;
+}
---
+++ soc/arm/aspeed/ast10x0/pinctrl_soc.h
@@ -0,0 +1,38 @@
+#ifndef ZEPHYR_SOC_ASPEED_AST10X0_PINCTRL_SOC_H_
+#define ZEPHYR_SOC_ASPEED_AST10X0_PINCTRL_SOC_H_
+
+#include <zephyr/types.h>
+
+#ifdef CONFIG_PINCTRL_ASPEED_STRING_NAME
+#define Z_PINCTRL_STATE_PIN_INIT(node_id, prop, idx)                                               \
+	{                                                                                          \
+		.sig_descs = DT_PROP(DT_PHANDLE_BY_IDX(node_id, prop, idx), sig_descs),            \
+		.num_of_descs = DT_PROP_LEN(DT_PHANDLE_BY_IDX(node_id, prop, idx), sig_descs),     \
+		.ball = DT_PROP(DT_PHANDLE_BY_IDX(node_id, prop, idx), ball),                      \
+		.name = DT_NODE_FULL_NAME(DT_PHANDLE_BY_IDX(node_id, prop, idx)),                  \
+	},
+#else
+#define Z_PINCTRL_STATE_PIN_INIT(node_id, prop, idx)                                               \
+	{                                                                                          \
+		.sig_descs = DT_PROP(DT_PHANDLE_BY_IDX(node_id, prop, idx), sig_descs),            \
+		.num_of_descs = DT_PROP_LEN(DT_PHANDLE_BY_IDX(node_id, prop, idx), sig_descs),     \
+		.ball = DT_PROP(DT_PHANDLE_BY_IDX(node_id, prop, idx), ball),                      \
+	},
+#endif
+
+#define Z_PINCTRL_STATE_PINS_INIT(node_id, prop)                                                   \
+	{                                                                                          \
+		DT_FOREACH_CHILD_VARGS(DT_PHANDLE(node_id, prop), DT_FOREACH_PROP_ELEM, pinmux,    \
+				       Z_PINCTRL_STATE_PIN_INIT)                                   \
+	}
+
+typedef struct pinctrl_soc_pin {
+	uint32_t pincfg;
+	int ball;
+#ifdef CONFIG_PINCTRL_ASPEED_STRING_NAME
+	char *name;
+#endif
+	int num_of_descs;
+	uint32_t sig_descs[3];
+} pinctrl_soc_pin_t;
+#endif
