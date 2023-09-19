4f7c366a25453c09523b39a75a77afe2ee342fdc
Tommy Huang <tommy_huang@aspeedtech.com>
2023-09-19 15:56:20 +0800

    subsys: shell: Add md / mw command
    Add memory read / memory write command into mem_service.c
    
    Signed-off-by: Tommy Huang <tommy_huang@aspeedtech.com>
    Change-Id: I44a114123a3563d34720bc02bb3d115cf6125af8

--- subsys/shell/modules/devmem_service.c
+++ subsys/shell/modules/devmem_service.c
@@ -15,6 +15,8 @@
 #else
 #include <zephyr/posix/unistd.h>
 #endif
+#include <string.h>
+#include <stdio.h>
 
 static inline bool is_ascii(uint8_t data)
 {
@@ -350,6 +352,46 @@ static int cmd_devmem(const struct shell *sh, size_t argc, char **argv)
 	return memory_write(sh, addr, width, value);
 }
 
+static int cmd_mem_wr(const struct shell *shell, size_t argc, char **argv)
+{
+	uint32_t addr, data;
+	char *endptr;
+
+	if (argc > 2) {
+		addr = strtoul(argv[1], &endptr, 16);
+		data = strtoul(argv[2], &endptr, 16);
+		*(uint32_t *)addr = data;
+	} else {
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int cmd_mem_md(const struct shell *shell, size_t argc, char **argv)
+{
+	uint32_t addr, length = 16;
+	char *endptr;
+	int i;
+
+	if (argc < 2)
+		return -EINVAL;
+
+	addr = strtoul(argv[1], &endptr, 16);
+	if (argc > 2)
+		length = strtoul(argv[2], &endptr, 16);
+
+	for (i = 0; i < length; i++) {
+		if ((i & 0x3) == 0)
+			printf("\n[%08x] ", addr + (i << 2));
+		printf("%08x ", *(uint32_t *)(addr + (i << 2)));
+	}
+
+	shell_print(shell, "\n");
+
+	return 0;
+}
+
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_devmem,
 			       SHELL_CMD_ARG(dump, NULL,
 					     "Usage:\n"
@@ -371,3 +413,5 @@ SHELL_CMD_REGISTER(devmem, &sub_devmem,
 		   "Write memory at address with mandatory width and value:\n"
 		   "devmem address <width> <value>",
 		   cmd_devmem);
+SHELL_CMD_REGISTER(md, NULL, "Mem Display command", cmd_mem_md);
+SHELL_CMD_REGISTER(mw, NULL, "Mem Write command", cmd_mem_wr);
