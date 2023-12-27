61c9d1aaa7ebf96017dd0cec64eb34341315d932
Kevin Chen <kevin_chen@aspeedtech.com>
2023-12-27 22:35:22 +0800

    driver: ipm: Add ast2600 driver code with sample.
    Change-Id: I55c62cc53dc0cd86e13f5d19ab952f3f2d556c7b
    Signed-off-by: Kevin Chen <kevin_chen@aspeedtech.com>

---
+++ drivers/ipm/ipm_ast2600.c
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) 2021 ASPEED Technology Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#define DT_DRV_COMPAT aspeed_ast2600_ipc
+
+#include <zephyr/drivers/ipm.h>
+#include <zephyr/sys/util.h>
+#include <zephyr/device.h>
+#include <zephyr/kernel.h>
+#include <zephyr/init.h>
+#include <zephyr/sys/sys_io.h>
+#include <zephyr/logging/log.h>
+#include <soc.h>
+#define LOG_LEVEL CONFIG_IPM_LOG_LEVEL
+LOG_MODULE_REGISTER(ipm_ast2600);
+
+/* secondary service processor ARM Cortex-M3 */
+#define IPCR_TRIG		0x18
+#define IPCR_STATUS		0x28
+#define IPCR_CLEAR		0x2c
+/* shared memory for ARM Cortex-M3 communicating with ARM Cortex-A7. */
+uint8_t shm_rx[CONFIG_IPC_SHM_RX_SIZE] NON_CACHED_SHM_RX = {0};
+uint8_t shm_tx[CONFIG_IPC_SHM_TX_SIZE] NON_CACHED_SHM_TX = {0};
+
+struct ipm_ast2600_config {
+	uint32_t base;
+	uint32_t num_irqs;
+	uint32_t shm_rx_size;
+	uint32_t shm_tx_size;
+};
+
+struct ipm_ast2600_obj {
+	ipm_callback_t callback;
+	void *user_data;
+};
+
+#define DEV_DATA(dev)		((struct ipm_ast2600_obj *)(dev)->data)
+#define DEV_CFG(dev)		((struct ipm_ast2600_config *)(dev)->config)
+
+static void ipc_ast2600_isr(const void *dev)
+{
+	struct ipm_ast2600_obj *obj = DEV_DATA((const struct device *)dev);
+	struct ipm_ast2600_config *config = DEV_CFG((const struct device *)dev);
+
+	uint32_t base = config->base;
+	uint32_t status;
+	int i;
+
+	status = sys_read32(base + IPCR_STATUS);
+
+	LOG_DBG("isr status: %08x\n", status);
+
+	if (!obj->callback) {
+		goto finish;
+	}
+
+	for (i = 0; i < config->num_irqs; i++) {
+		if (status & BIT(i)) {
+			obj->callback(dev, obj->user_data, i, NULL);
+		}
+	}
+
+finish:
+	sys_write32(status, base + IPCR_CLEAR);
+}
+
+static int ipm_ast2600_send(const struct device *dev, int wait, uint32_t id, const void *data,
+			    int size)
+{
+	struct ipm_ast2600_config *config = DEV_CFG(dev);
+	uint32_t base = config->base;
+	uint32_t shm_tx_size = config->shm_tx_size;
+	uint32_t ret = 0;
+	uint32_t reg;
+
+	if (id >= config->num_irqs) {
+		ret = -EINVAL;
+		goto finish;
+	}
+
+	if (size > shm_tx_size) {
+		ret = -EINVAL;
+		goto finish;
+	} else {
+		memcpy((void *)shm_tx, data, size);
+	}
+
+	reg = sys_read32(base + IPCR_TRIG);
+	if (reg & BIT(id)) {
+		ret = -EBUSY;
+		goto finish;
+	}
+
+	sys_write32(BIT(id), base + IPCR_TRIG);
+	if (wait) {
+		do {
+			k_busy_wait(100);
+			reg = sys_read32(base + IPCR_TRIG);
+		} while (reg & BIT(id));
+	}
+
+finish:
+	return ret;
+}
+
+static uint32_t ipm_ast2600_max_id_val_get(const struct device *dev)
+{
+	struct ipm_ast2600_config *config = DEV_CFG(dev);
+
+	return config->num_irqs;
+}
+
+static int ipm_ast2600_max_data_size_get(const struct device *dev)
+{
+	struct ipm_ast2600_config *config = DEV_CFG(dev);
+
+	/* share memory include TX and RX data. */
+	return config->shm_rx_size;
+}
+
+static int ipm_ast2600_set_enabled(const struct device *dev, int enable)
+{
+	ARG_UNUSED(dev);
+	ARG_UNUSED(enable);
+	return 0;
+}
+
+static void ipm_ast2600_register_callback(const struct device *dev, ipm_callback_t cb,
+					  void *user_data)
+{
+	struct ipm_ast2600_obj *obj = DEV_DATA(dev);
+
+	obj->user_data = user_data;
+	obj->callback = cb;
+}
+
+static const struct ipm_driver_api ipm_ast2600_driver_api = {
+	.send = ipm_ast2600_send,
+	.register_callback = ipm_ast2600_register_callback,
+	.max_data_size_get = ipm_ast2600_max_data_size_get,
+	.max_id_val_get = ipm_ast2600_max_id_val_get,
+	.set_enabled = ipm_ast2600_set_enabled,
+};
+
+#define IPM_AST2600_IRQ_CONNECT(index, inst)                                                       \
+	do {                                                                                       \
+		IRQ_CONNECT(DT_INST_IRQ_BY_IDX(inst, index, irq),                                  \
+			    DT_INST_IRQ_BY_IDX(inst, index, priority), ipc_ast2600_isr,            \
+			    DEVICE_DT_INST_GET(inst), 0);                                          \
+		irq_enable(DT_INST_IRQN(inst));                                                    \
+	} while (false);
+
+#define IPM_AST2600_INIT(n)                                                                        \
+	static int ipm_ast2600_config_func_##n(const struct device *dev);                          \
+	static const struct ipm_ast2600_config ipm_ast2600_config_##n = {                          \
+		.base = DT_INST_REG_ADDR(n),                                                       \
+		.num_irqs = DT_NUM_IRQS(DT_DRV_INST(n)),                                           \
+		.shm_rx_size = DT_REG_SIZE(DT_PROP(DT_DRV_INST(n), rx_region)),                   \
+		.shm_tx_size = DT_REG_SIZE(DT_PROP(DT_DRV_INST(n), tx_region))};                   \
+	static struct ipm_ast2600_obj ipm_ast2600_obj_##n;                                         \
+	DEVICE_DT_INST_DEFINE(n, &ipm_ast2600_config_func_##n, NULL, &ipm_ast2600_obj_##n,         \
+			      &ipm_ast2600_config_##n, POST_KERNEL,                                \
+			      CONFIG_KERNEL_INIT_PRIORITY_DEVICE, &ipm_ast2600_driver_api);        \
+	static int ipm_ast2600_config_func_##n(const struct device *dev)                           \
+	{                                                                                          \
+		LISTIFY(DT_NUM_IRQS(DT_DRV_INST(n)), IPM_AST2600_IRQ_CONNECT, (), n);              \
+                                                                                                   \
+		return 0;                                                                          \
+	}
+DT_INST_FOREACH_STATUS_OKAY(IPM_AST2600_INIT)
--- samples/drivers/ipm/ipm_aspeed/src/main.c
+++ samples/drivers/ipm/ipm_aspeed/src/main.c
@@ -2,10 +2,36 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/drivers/ipm.h>
 
+#if defined(CONFIG_SOC_AST2600)
+extern uint8_t *shm_rx, *shm_tx;
+#else
 #define DEFAULT_LINE_LENGTH_BYTES (16)
+#endif
 static void test_ipm_cb(const struct device *ipmdev, void *user_data,
 			       uint32_t id, volatile void *data)
 {
+#if defined(CONFIG_SOC_AST2600)
+	int i;
+	uint32_t shared_memory_rx = (uint32_t)&shm_rx;
+	uint32_t shared_memory_tx = (uint32_t)&shm_tx;
+
+	printk("[CM3] rx addr:%08x, tx addr:%08x\n", shared_memory_rx, shared_memory_tx);
+	/* Check the CM3 RX data from CA7 RX data. */
+	printk("CM3 RX data from CA7 TX data:\n");
+	for (i = 0; i < CONFIG_IPC_SHM_RX_SIZE; i += 4) {
+		if (sys_read32(shared_memory_rx + i) != 0) {
+			if (((i >> 2) & 0x3) == 0x0)
+				printk("[%08x] ", shared_memory_rx + i);
+			printk("%08x ", sys_read32(shared_memory_rx + i));
+			if (((i >> 2) & 0x3) == 0x3)
+				printk("\n");
+		} else
+			break;
+	}
+
+	/* Write back to CM3 TX from CM3 RX data. */
+	ipm_send(ipmdev, 1, 0, (void *)shared_memory_rx, CONFIG_IPC_SHM_TX_SIZE);
+#else
 	int i;
 	int check = 0;
 	int width = 4;
@@ -34,12 +60,22 @@ static void test_ipm_cb(const struct device *ipmdev, void *user_data,
 		}
 	else
 		printk("Check msg data: pass.\n");
+#endif
 }
 
 void main(void)
 {
+	int rc;
 	const struct device *ipmdev;
 
+#if defined(CONFIG_SOC_AST2600)
+	/* setup IPM */
+	ipmdev = device_get_binding("ipc@7e6c0000");
+	if (!ipmdev) {
+		printk("device_get_binding failed to find device\n");
+		return;
+	}
+#elif defined(CONFIG_SOC_AST2700)
 #if defined(CONFIG_BOARD_AST2700_SSP_FPGA)
 	printk("SSP alive.\n");
 	ipmdev = device_get_binding("ipc@0");
@@ -80,8 +116,17 @@ void main(void)
 		while (1) {
 		}
 	}
+#endif
+	/* setup callback function with received data processing. */
 	ipm_register_callback(ipmdev, test_ipm_cb, NULL);
-	ipm_set_enabled(ipmdev, 1);
+
+	/* enable ipm. */
+	rc = ipm_set_enabled(ipmdev, 1);
+	if (rc) {
+		printk("cannot ipm_set_enabled\n");
+		return;
+	}
+
 	printk("IPM initialized\n");
 
 	while (1) {
