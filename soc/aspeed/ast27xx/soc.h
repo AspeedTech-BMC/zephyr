53d25d17ad54efb32a2ad5aa6e5f292f99379525
Dylan Hung <dylan_hung@aspeedtech.com>
2024-11-14 17:57:08 +0800

    soc: aspeed: Rename SoC series AST27X0 to AST27XX
    Revise naming convention from AST27X0 to AST27XX.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: Icadba8aae8c2171f71ea631f3720d2449d0d0ae3

--- soc/aspeed/ast27xx/soc.h
+++ soc/aspeed/ast27xx/soc.h
@@ -4,12 +4,12 @@
  * Copyright (c) 2023 ASPEED Technology Inc.
  */
 
-#ifndef ZEPHYR_SOC_ASPEED_AST27X0_SOC_H_
-#define ZEPHYR_SOC_ASPEED_AST27X0_SOC_H_
+#ifndef ZEPHYR_SOC_ASPEED_AST27XX_SOC_H_
+#define ZEPHYR_SOC_ASPEED_AST27XX_SOC_H_
 #include <aspeed_util.h>
 
 #ifdef CONFIG_ARM
 #include <cmsis_core_m_defaults.h>
 #endif /* ARM */
 
-#endif /* ZEPHYR_SOC_ASPEED_AST27X0_SOC_H_*/
+#endif /* ZEPHYR_SOC_ASPEED_AST27XX_SOC_H_*/
