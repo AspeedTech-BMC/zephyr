9f9465d4f85cf01d6115170838ebc6b9af2b695c
Tommy Haung <tommy_huang@aspeedtech.com>
2024-11-21 17:02:25 +0800

    drivers: i2c: Remove unnecessary RX length clear
    The DMA RX length will be cleared when TX/RX done.
    Or it will be cleared by SW. Remove it for unnecessary
    sw clear.
    
    Signed-off-by: Tommy Haung <tommy_huang@aspeedtech.com>
    Change-Id: I13eed59ebc1cfcfd29cf329e1345ef35e387bf05
    (cherry picked from commit 3e07e7788f04da49bfcb01769159c740c06860bb)

--- drivers/i2c/i2c_aspeed.c
+++ drivers/i2c/i2c_aspeed.c
@@ -1329,7 +1329,6 @@ void aspeed_i2c_slave_packet_irq(const struct device *dev, uint32_t i2c_base, ui
 				}
 			}
 
-			sys_write32(0, i2c_base + AST_I2CS_DMA_LEN_STS);
 			sys_write32(AST_I2CS_SET_RX_DMA_LEN(I2C_SLAVE_BUF_SIZE)
 			, i2c_base + AST_I2CS_DMA_LEN);
 		} else if (config->mode == BUFF_MODE) {
@@ -1396,7 +1395,6 @@ void aspeed_i2c_slave_packet_irq(const struct device *dev, uint32_t i2c_base, ui
 			}
 			LOG_DBG("tx [%02x]", data->slave_dma_buf[0]);
 
-			sys_write32(0, i2c_base + AST_I2CS_DMA_LEN_STS);
 			sys_write32(AST_I2CS_SET_TX_DMA_LEN(1)
 			, i2c_base + AST_I2CS_DMA_LEN);
 		} else if (config->mode == BUFF_MODE) {
@@ -1486,7 +1484,6 @@ void aspeed_i2c_slave_packet_irq(const struct device *dev, uint32_t i2c_base, ui
 				, &data->slave_dma_buf[0]);
 			}
 			LOG_DBG("rx : [%02x]", data->slave_dma_buf[0]);
-			sys_write32(0, i2c_base + AST_I2CS_DMA_LEN_STS);
 			sys_write32(AST_I2CS_SET_TX_DMA_LEN(1)
 			, i2c_base + AST_I2CS_DMA_LEN);
 		} else if (config->mode == BUFF_MODE) {
@@ -1518,7 +1515,6 @@ void aspeed_i2c_slave_packet_irq(const struct device *dev, uint32_t i2c_base, ui
 		}
 		if (config->mode == DMA_MODE) {
 			cmd |= AST_I2CS_RX_DMA_EN;
-			sys_write32(0, i2c_base + AST_I2CS_DMA_LEN_STS);
 			sys_write32(AST_I2CS_SET_RX_DMA_LEN(I2C_SLAVE_BUF_SIZE)
 			, i2c_base + AST_I2CS_DMA_LEN);
 		} else if (config->mode == BUFF_MODE) {
