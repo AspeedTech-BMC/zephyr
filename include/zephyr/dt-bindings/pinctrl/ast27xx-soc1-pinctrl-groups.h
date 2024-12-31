43e15b076dee6f2375b3749f0f5737cf87022245
Billy Tsai <billy_tsai@aspeedtech.com>
2024-12-31 13:59:24 +0800

    dts: bindings: pinctrl: Add binding for Aspeed AST27xx soc1 pinctrl
    - Enum the ball names
    - Add the uart0 TX/RX pin configuration sample
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: I3eda57dee0381c8619091662809e2f2524e3e36e

---
+++ include/zephyr/dt-bindings/pinctrl/ast27xx-soc1-pinctrl-groups.h
@@ -0,0 +1,4 @@
+#ifndef _AST27XX_SOC1_PINCTRL_GROUPS_H_
+#define _AST27XX_SOC1_PINCTRL_GROUPS_H_
+#define pinctrl_uart0_default   &pin_txd0 &pin_rxd0
+#endif /* end of "#ifndef _AST27XX_SOC1_PINCTRL_GROUPS_H_" */
