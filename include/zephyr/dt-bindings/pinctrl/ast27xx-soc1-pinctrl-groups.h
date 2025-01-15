51b75b83909df940f0fa129cd8e9fd57187fab87
Dylan Hung <dylan_hung@aspeedtech.com>
2025-01-15 17:24:51 +0800

    dts: arm: ast27xx: Add UART7 node and its pin control
    Add UART7 node and also add pin control related nodes for both UART6 and
    UART7.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: I23f50050d467f619838281abc84b2397bbd28aa1

--- include/zephyr/dt-bindings/pinctrl/ast27xx-soc1-pinctrl-groups.h
+++ include/zephyr/dt-bindings/pinctrl/ast27xx-soc1-pinctrl-groups.h
@@ -1,4 +1,6 @@
 #ifndef _AST27XX_SOC1_PINCTRL_GROUPS_H_
 #define _AST27XX_SOC1_PINCTRL_GROUPS_H_
 #define pinctrl_uart0_default   &pin_txd0 &pin_rxd0
+#define pinctrl_uart6_default   &pin_txd6 &pin_rxd6
+#define pinctrl_uart7_default   &pin_txd7 &pin_rxd7
 #endif /* end of "#ifndef _AST27XX_SOC1_PINCTRL_GROUPS_H_" */
