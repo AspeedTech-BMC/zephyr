f7d3b3468e77dcb62ac5083cca29de061976fcdf
Kevin Chen <kevin_chen@aspeedtech.com>
2025-03-26 10:50:56 +0800

    drivers: ipm: Update logging to use LOG_INF and LOG_ERR
    Use logging without printk.
    
    Change-Id: Id6bd52a6d253c73bac45a9779b94e937765e36b3
    Signed-off-by: Kevin Chen <kevin_chen@aspeedtech.com>

--- samples/boards/ast2700_evb/demo/src/ipc_sample.c
+++ samples/boards/ast2700_evb/demo/src/ipc_sample.c
@@ -7,8 +7,8 @@
 #include <soc.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/ipm.h>
-#include <zephyr/sys/printk.h>
 #include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(ipc_test, CONFIG_SOC_LOG_LEVEL);
 
 #define DEFAULT_LINE_LENGTH_BYTES (16)
 void test_ipm_cb(const struct device *ipmdev, void *user_data,
@@ -20,13 +20,13 @@ void test_ipm_cb(const struct device *ipmdev, void *user_data,
 	int max_msg_data_size = ipm_max_data_size_get(ipmdev);
 	uint32_t *buf = (uint32_t *)msg_data;
 
-	printk("%s:msg id %x, msg data at %p, msg size 0x%x\n",
-	       __func__, id, msg_data, max_msg_data_size);
+	LOG_INF("%s:msg id %x, msg data at %p, msg size 0x%x\n",
+		__func__, id, msg_data, max_msg_data_size);
 	while (max_msg_data_size) {
-		printk("%p:", buf);
+		LOG_INF("%p:", buf);
 
 		for (i = 0; i < linelen; i++)
-			printk(" %08x ", buf[i]);
+			LOG_INF(" %08x ", buf[i]);
 		printk("\n");
 		buf += linelen;
 		max_msg_data_size -= linelen * width;
@@ -42,7 +42,7 @@ int ipc_test(void)
 	strcpy(ipc_name, "ipc1@400");
 	ipmdev = device_get_binding(&ipc_name[0]);
 	if (!ipmdev) {
-		printk("%s: device_get_binding failed to find device\n", ipc_name);
+		LOG_ERR("%s: device_get_binding failed to find device\n", ipc_name);
 		rc = 1;
 		goto fail;
 	}
@@ -52,7 +52,7 @@ int ipc_test(void)
 	ipm_register_id_callback(ipmdev, device_id, (void *)test_ipm_cb, NULL);
 	rc = ipm_set_id_enabled(ipmdev, device_id, enable);
 	if (rc) {
-		printk("%s: cannot ipm_set_enabled\n", ipc_name);
+		LOG_ERR("%s: cannot ipm_set_enabled\n", ipc_name);
 		rc = 1;
 		goto fail;
 	}
