26405119062e548ce5cb16ab770a2e2adee8da1a
Kevin Chen <kevin_chen@aspeedtech.com>
2025-09-08 16:07:57 +0800

    soc: add support for ASPEED AST1040 SoC
    Introduce initial support for the ASPEED AST1040 SoC, including device
    tree, Kconfig, and IRQ definitions. Add linker scripts, SoC-specific
    initialization, and configuration files to support Zephyr integration.
    
    This enables basic functionality and prepares for further development
    on the AST1040 platform.
    
    Change-Id: I24a1cefda9a52dad08fd96f1ea7f54d66ef88211
    Signed-off-by: Kevin Chen <kevin_chen@aspeedtech.com>

---
+++ soc/aspeed/ast1040/soc.c
@@ -0,0 +1,27 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2023 ASPEED Technology Inc.
+ */
+
+#include <zephyr/init.h>
+#include <zephyr/kernel.h>
+#include <stdint.h>
+#include <zephyr/linker/linker-defs.h>
+#include <zephyr/device.h>
+#include <zephyr/cache.h>
+#include <string.h>
+#include <soc.h>
+
+#define SCU1_REG		DT_REG_ADDR(DT_NODELABEL(syscon1))
+#define SCU1_RSTLOG0		(SCU1_REG + 0x050)
+#define   SCU1_RSTLOG0_SRST	BIT(0)
+
+extern char __RAM_NC_start[];
+extern char __RAM_NC_end[];
+
+void z_arm_platform_init(void)
+{
+	/* clear non-cached .bss */
+	(void)memset(__RAM_NC_start, 0, __RAM_NC_end - __RAM_NC_start);
+}
---
+++ soc/aspeed/ast1040/soc.h
@@ -0,0 +1,20 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (c) 2023 ASPEED Technology Inc.
+ */
+
+#ifndef ZEPHYR_SOC_ASPEED_AST27XX_SOC_H_
+#define ZEPHYR_SOC_ASPEED_AST27XX_SOC_H_
+#include <aspeed_util.h>
+
+#ifdef CONFIG_ARM
+#include <cmsis_core_m_defaults.h>
+#endif /* ARM */
+
+uintptr_t ast27xx_soc_phy_addr_to_virt_addr(uint64_t addr);
+uint64_t ast27xx_soc_virt_addr_to_phy_addr(uintptr_t addr);
+#define TO_PHY_ADDR(addr)		ast27xx_soc_virt_addr_to_phy_addr(addr)
+#define TO_VIR_ADDR(addr)		ast27xx_soc_phy_addr_to_virt_addr(addr)
+
+#endif /* ZEPHYR_SOC_ASPEED_AST27XX_SOC_H_*/
