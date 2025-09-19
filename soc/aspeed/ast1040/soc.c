6c45942e3d12e012eaebb14fdf47b9a055583be6
Kevin Chen <kevin_chen@aspeedtech.com>
2025-09-19 02:43:48 +0000

    Revert "soc: ast1040: Set BMC UART selection in SCU1_MISC register"
    This reverts commit c0458c7a801b430f542590171bda96f5c22cf034.
    
    Reason for revert:It is a WA for the wrong SCU setting.
    
    Change-Id: I40968e0f98e19786011f4bd695190d6c7fec513f
    Signed-off-by: Kevin Chen <kevin_chen@aspeedtech.com>

--- soc/aspeed/ast1040/soc.c
+++ soc/aspeed/ast1040/soc.c
@@ -13,11 +13,9 @@
 #include <string.h>
 #include <soc.h>
 
-#define SCU1_REG		DT_REG_ADDR(DT_NODELABEL(syscon))
+#define SCU1_REG		DT_REG_ADDR(DT_NODELABEL(syscon1))
 #define SCU1_RSTLOG0		(SCU1_REG + 0x050)
 #define   SCU1_RSTLOG0_SRST	BIT(0)
-#define SCU1_MISC		(SCU1_REG + 0x0c0)
-#define   SCU1_MISC_UARTBMC_SEL	BIT(20)
 
 extern char __RAM_NC_start[];
 extern char __RAM_NC_end[];
@@ -26,7 +24,4 @@ void z_arm_platform_init(void)
 {
 	/* clear non-cached .bss */
 	(void)memset(__RAM_NC_start, 0, __RAM_NC_end - __RAM_NC_start);
-
-	/* TODO: Set SCU_UARTBMC_SEL to BMC UART. This is a WA. */
-	sys_write32(SCU1_MISC_UARTBMC_SEL, SCU1_MISC);
 }
