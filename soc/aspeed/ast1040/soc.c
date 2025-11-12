ee36f7b5c55b2ed98191d9b1810c9bfcce45ba59
Cool Lee <cool_lee@aspeedtech.com>
2025-11-12 11:59:52 +0800

    soc: ast1040: Fix build error
    Add missing functions back.
    
    Signed-off-by: Cool Lee <cool_lee@aspeedtech.com>
    Change-Id: Iba1e69acaa15e56f77cde4326f266bba975722ab

--- soc/aspeed/ast1040/soc.c
+++ soc/aspeed/ast1040/soc.c
@@ -25,3 +25,24 @@ void z_arm_platform_init(void)
 	/* clear non-cached .bss */
 	(void)memset(__RAM_NC_start, 0, __RAM_NC_end - __RAM_NC_start);
 }
+
+/* DMA address translation */
+#if defined(CONFIG_SOC_AST1040_BOOTMCU)
+uint64_t ast27xx_soc_virt_addr_to_phy_addr(uintptr_t addr)
+{
+	if (addr < 0x80000000) {
+		return addr;
+	}
+
+	return (uint64_t)(addr & ~0x80000000) | 0x400000000ULL;
+}
+
+uintptr_t ast27xx_soc_phy_addr_to_virt_addr(uint64_t addr)
+{
+	if (addr < 0x400000000) {
+		return addr;
+	}
+
+	return (uintptr_t)(addr - 0x400000000ULL) | 0x80000000UL;
+}
+#endif
