12912ce9bef47f0ad9c4cc93be44edfc8092f56d
Billy Tsai <billy_tsai@aspeedtech.com>
2026-02-10 15:04:31 +0800

    pinctrl: aspeed: Add pin and group definitions for SPI and FW SPI
    Add pin control nodes and group macros for SPI0, SPI1, SPI2, and firmware
    SPI quad pins to support configuration of SPI controller and firmware
    interface signals on AST27xx.
    
    Change-Id: I820a3b657947d187080e89e5e5117b9831911b2c
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>

--- include/zephyr/dt-bindings/pinctrl/ast27xx-soc1-pinctrl-groups.h
+++ include/zephyr/dt-bindings/pinctrl/ast27xx-soc1-pinctrl-groups.h
@@ -3,4 +3,14 @@
 #define pinctrl_uart0_default   &pin_txd0 &pin_rxd0
 #define pinctrl_uart6_default   &pin_txd6 &pin_rxd6
 #define pinctrl_uart7_default   &pin_txd7 &pin_rxd7
+#define pinctrl_fwspi_quad_default &pin_fwspidq2 &pin_fwspidq3
+#define pinctrl_spi0_default &pin_spi0ck &pin_spi0mosi &pin_spi0miso
+#define pinctrl_spi0_quad_default &pin_spi0dq2 &pin_spi0dq3
+#define pinctrl_spi0_cs1_default &pin_spi0cs1
+#define pinctrl_spi1_default &pin_spi1ck &pin_spi1mosi &pin_spi1miso
+#define pinctrl_spi1_quad_default &pin_spi1dq2 &pin_spi1dq3
+#define pinctrl_spi1_cs1_default &pin_spi1cs1
+#define pinctrl_spi2_default &pin_spi2ck &pin_spi2mosi &pin_spi2miso
+#define pinctrl_spi2_quad_default &pin_spi2dq2 &pin_spi2dq3
+#define pinctrl_spi2_cs1_default &pin_spi2cs1
 #endif /* end of "#ifndef _AST27XX_SOC1_PINCTRL_GROUPS_H_" */
