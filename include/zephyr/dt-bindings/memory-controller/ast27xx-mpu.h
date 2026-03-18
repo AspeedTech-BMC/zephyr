499c2d2dbe8a3f4dc73472fd9b709958b246a3dd
chengyu_lin <chengyu_lin@aspeedtech.com>
2026-03-18 18:25:06 +0800

    dts: bindings: mpu: Update memory protection master id
    Add the master id that protected by privilege control.
    Update some master name that conflict with cpu die master.
    
    Signed-off-by: chengyu_lin <chengyu_lin@aspeedtech.com>
    Change-Id: I94f39984f481d74144f1f52ba5bade43bee395f5

--- include/zephyr/dt-bindings/memory-controller/ast27xx-mpu.h
+++ include/zephyr/dt-bindings/memory-controller/ast27xx-mpu.h
@@ -47,13 +47,59 @@
 
 /* port 4 */
 #define MPU_ID_XDMA		(33)
-#define MPU_ID_SDIO		(34)
+#define MPU_ID_EMMC		(34)
 #define MPU_ID_SLIM		(35)
 
 /* port 5 */
 #define MPU_ID_USBH_A		(36)
 #define MPU_ID_USBH_B		(37)
 #define MPU_ID_UFS		(38)
+
+/* port h2m0 */
+#define MPU_ID_AHBC_MONITOR	(0x1006)
+#define MPU_ID_SBDMA		(0x1007)
+#define MPU_ID_UARTDBG0		(0x1008)
+#define MPU_ID_PORT80		(0x1009)
+#define MPU_ID_DP_MCU		(0x100A)
+#define MPU_ID_EMMC_BOOT	(0x100C)
+#define MPU_ID_UDMAC		(0x100E)
+
+/* port h2m1 */
+#define MPU_ID_BOOTMCU		(0x2001)
+#define MPU_ID_FMC		(0x2004)
+#define MPU_ID_AHBC1		(0x2006)
+#define MPU_ID_SPI0		(0x2007)
+#define MPU_ID_SPI1		(0x2008)
+#define MPU_ID_SPI2		(0x2009)
+#define MPU_ID_ESPI0		(0x200A)
+#define MPU_ID_ESPI1		(0x200B)
+#define MPU_ID_80H0		(0x200E)
+#define MPU_ID_80H1		(0x200F)
+#define MPU_ID_UARTDBG1		(0x2010)
+#define MPU_ID_H2A_SPI1		(0x2013)
+#define MPU_ID_H2A_SPI2		(0x2014)
+#define MPU_ID_UARTDMA		(0x2015)
+#define MPU_ID_USB2UARTA	(0x201A)
+#define MPU_ID_USB2UARTB	(0x201B)
+
+/* port sli */
+#define MPU_ID_I2C		(0x4000)
+#define MPU_ID_I2C_FILTER	(0x4001)
+#define MPU_ID_SDIO		(0x4002)
+#define MPU_ID_SPI_FILTER	(0x4003)
+#define MPU_ID_ACE		(0x4004)
+#define MPU_ID_UHCI_IO		(0x4005)
+#define MPU_ID_E2M_IO		(0x4006)
+#define MPU_ID_H2M1		(0x4007)
+#define MPU_ID_MAC0		(0x4008)
+#define MPU_ID_MAC1		(0x4009)
+#define MPU_ID_MAC2		(0x400A)
+#define MPU_ID_FSI		(0x400B)
+#define MPU_ID_MCTP2		(0x400C)
+#define MPU_ID_LTPI0		(0x400D)
+#define MPU_ID_USB2_PORT_A	(0x400E)
+#define MPU_ID_USB2_PORT_B	(0x400F)
+#define MPU_ID_LTPI1		(0x4010)
 
 #define S_READWRITE		0
 #define S_READONLY		1
