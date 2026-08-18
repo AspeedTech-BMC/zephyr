8b8631ec6db22ea0aac2b924775b7cbf036ab988
Billy Tsai <billy_tsai@aspeedtech.com>
2026-08-18 16:02:24 +0800

    jtag: add support for running TCK clock cycles
    Adds a new shell command `tck_run` to control the JTAG TCK clock for a
    specified number of cycles.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: I230d4f4f21261719a9a32aaf48771e7d8a085d71

--- drivers/jtag/jtag_shell.c
+++ drivers/jtag/jtag_shell.c
@@ -106,6 +106,27 @@ static int cmd_frequency(const struct shell *shell, size_t argc, char **argv)
 	return 0;
 }
 
+static int cmd_tck_run(const struct shell *shell, size_t argc, char **argv)
+{
+	const struct device *dev;
+	uint32_t tck;
+	int err;
+
+	dev = device_get_binding(argv[-1]);
+	if (!dev) {
+		shell_error(shell, "JTAG device not found");
+		return -EINVAL;
+	}
+	tck = strtoul(argv[1], NULL, 0);
+	err = jtag_tck_run(dev, tck);
+	if (err) {
+		shell_error(shell, "failed to run JTAG TCK (err %d)",
+			    err);
+		return err;
+	}
+	return 0;
+}
+
 static int cmd_tap_state(const struct shell *shell, size_t argc, char **argv, void *data)
 {
 	const struct device *dev;
@@ -173,6 +194,7 @@ SHELL_STATIC_SUBCMD_SET_CREATE(sub_action_cmds, SHELL_CMD(high, &sub_pin_cmds, "
 
 SHELL_STATIC_SUBCMD_SET_CREATE(sub_jtag_cmds,
 			       SHELL_CMD_ARG(frequency, NULL, "<frequency>", cmd_frequency, 2, 0),
+			       SHELL_CMD_ARG(tck_run, NULL, "<run_count>", cmd_tck_run, 2, 0),
 			       SHELL_CMD_ARG(ir_scan, NULL, "<len> <value>", cmd_ir_scan, 3, 0),
 			       SHELL_CMD_ARG(dr_scan, NULL, "<len> <value>", cmd_dr_scan, 3, 0),
 			       SHELL_CMD(tap_set, &sub_tap_cmds, "<tap_state>", NULL),
